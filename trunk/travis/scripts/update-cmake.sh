b2d1c567e0240a65c91f5ca98e693c02f4e2e8ca
jrobble <jrobble@mitre.org>
2016-12-14 15:42:06 -0500

    Added cmake repo.

--- trunk/travis/scripts/update-cmake.sh
+++ trunk/travis/scripts/update-cmake.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 set -ex
+
 cmake --version
+
+# Ubuntu 12.04.5 LTS comes with cmake 2.8.7, we need 2.8.12.2 or later
+sudo add-apt-repository --yes ppa:kalakris/cmake
 sudo apt-get update
 sudo apt-get install cmake
+
+cmake --version

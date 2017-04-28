657979830d6a6363d135f831871a6875977c0029
brosenberg <brosenberg@mitre.org>
2017-04-28 17:32:48 -0400

    Comment out pipelines2 link

--- trunk/workflow-manager/src/main/webapp/resources/mytheme/js/angular_app/app.js
+++ trunk/workflow-manager/src/main/webapp/resources/mytheme/js/angular_app/app.js
@@ -40,7 +40,7 @@ var App = angular.module('mpf.wfm', [
 	'angular-confirm',
 	'mpf.wfm.controller.ServerMediaCtrl',
 	'mpf.wfm.controller.AdminComponentRegistrationCtrl',
-	'mpf.wfm.pipeline2',
+	// 'mpf.wfm.pipeline2',
 	'mpf.wfm.filters',
 	'mpf.wfm.services',
 	'mpf.wfm.directives',
@@ -77,12 +77,14 @@ App.config(['$stateProvider', '$urlRouterProvider','$httpProvider' ,function ($s
 		  controller: PipelinesCtrl
 	  });
 
+	  /*
 	  $stateProvider.state('/pipelines2', {
 		  url: '/pipelines2',
 		  templateUrl: 'pipelines2/layout.html',
           controller: 'Pipelines2Ctrl',
 		  controllerAs: 'pipes2'
 	  });
+	  */
 
 	  $stateProvider.state('/jobs', {
 		  url: '/jobs',

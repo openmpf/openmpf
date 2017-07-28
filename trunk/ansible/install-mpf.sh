75646fb43901356ea8e7abd6d5bf791f71f56c61
jrobble <jrobble@mitre.org>
2017-07-28 09:36:52 -0400

    Updated semantic version to 0.10.0.

--- trunk/ansible/install-mpf.sh
+++ trunk/ansible/install-mpf.sh
@@ -26,7 +26,7 @@
 
 MPF_MANAGEMENT_PATH="/opt/mpf/manage/"
 echo "--------------------------------------------------------------------------------"
-echo "                           Installing OpenMPF 0.9.0"
+echo "                           Installing OpenMPF 0.10.0"
 echo "--------------------------------------------------------------------------------"
 if [ "$EUID" -ne 0 ]
   then echo "Please run as sudo/root. Exiting..."

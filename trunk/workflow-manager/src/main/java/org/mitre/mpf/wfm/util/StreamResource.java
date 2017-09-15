46787b69be6a5212f86f7570de28cdd415772dfe
djv <djv@mitre.org>
2017-09-15 15:17:32 -0400

    addressed review discussion, 3 items

--- trunk/workflow-manager/src/main/java/org/mitre/mpf/wfm/util/StreamResource.java
+++ trunk/workflow-manager/src/main/java/org/mitre/mpf/wfm/util/StreamResource.java
@@ -1,3 +1,29 @@
+/******************************************************************************
+ * NOTICE                                                                     *
+ *                                                                            *
+ * This software (or technical data) was produced for the U.S. Government     *
+ * under contract, and is subject to the Rights in Data-General Clause        *
+ * 52.227-14, Alt. IV (DEC 2007).                                             *
+ *                                                                            *
+ * Copyright 2017 The MITRE Corporation. All Rights Reserved.                 *
+ ******************************************************************************/
+
+/******************************************************************************
+ * Copyright 2017 The MITRE Corporation                                       *
+ *                                                                            *
+ * Licensed under the Apache License, Version 2.0 (the "License");            *
+ * you may not use this file except in compliance with the License.           *
+ * You may obtain a copy of the License at                                    *
+ *                                                                            *
+ *    http://www.apache.org/licenses/LICENSE-2.0                              *
+ *                                                                            *
+ * Unless required by applicable law or agreed to in writing, software        *
+ * distributed under the License is distributed on an "AS IS" BASIS,          *
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.   *
+ * See the License for the specific language governing permissions and        *
+ * limitations under the License.                                             *
+ ******************************************************************************/
+
 package org.mitre.mpf.wfm.util;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
@@ -12,7 +38,7 @@ import org.mitre.mpf.wfm.enums.UriScheme;
 public class StreamResource {
 
     public static final String NO_ERROR = "NO ERROR";
-    public static final String NOT_DEFINED_URI_SCHEME = "URI scheme not well defined";
+    public static final String NOT_DEFINED_URI_SCHEME = "URI scheme not defined";
     public static final String NOT_SUPPORTED_URI_SCHEME = "Unsupported URI scheme";
 
     private String uri;

d23e6f08752306667207f10bc1c045d5a9384387
djv <djv@mitre.org>
2017-12-22 13:37:06 -0500

    NewAlertCallbackUri and callbackMethod no longer required to create streaming job

--- trunk/mpf-system-tests/src/main/java/org/mitre/mpf/mst/TestSystem.java
+++ trunk/mpf-system-tests/src/main/java/org/mitre/mpf/mst/TestSystem.java
@@ -236,7 +236,7 @@ public abstract class TestSystem {
 				Collections.emptyMap(), jobProperties,
 				buildOutput, priority,
 				stallTimeout,
-				null,null,null,null);
+				null,null);
 		long jobRequestId = mpfService.submitJob(jsonStreamingJobRequest);
 		Assert.assertTrue(waitFor(jobRequestId));
 		return jobRequestId;

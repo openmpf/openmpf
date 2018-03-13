396aa84f0b3d5ed7a1acf7e8622bc986c993d047
jrobble <jrobble@mitre.org>
2018-03-13 13:19:12 -0400

    Fleshed out add-node command.

--- trunk/bin/mpf-scripts/mpf/mpf_nodes.py
+++ trunk/bin/mpf-scripts/mpf/mpf_nodes.py
@@ -27,10 +27,12 @@
 import argh
 import base64
 import getpass
+import os.path
 import string
 import urllib2
 
-import mpf_sys
+from subprocess import call
+
 import mpf_util
 
 
@@ -42,15 +44,22 @@ import mpf_util
 def add_node(node, port=None, all_mpf_nodes=None, workflow_manager_url=None):
     """ Adds a spare node to the Workflow Manager """
 
-    known_nodes = []
+    nodes_list = []
     for known_node_with_port in all_mpf_nodes.split(','):
-        known_nodes.append(known_node_with_port.split('[')[0])
+        known_node = known_node_with_port.split('[')[0]
+        if known_node:
+            nodes_list.append(known_node)
 
     # Check if node is already in all-mpf-nodes
-    if node in known_nodes:
-        print mpf_util.MsgUtil.red('Child node: %s is already in the list of known nodes: %s' % (node, known_nodes))
-        print mpf_util.MsgUtil.red('Child node: %s has not been added' % node)
+    if node in nodes_list:
+        print mpf_util.MsgUtil.red('Child node %s is already in the list of known nodes: %s' % (node, known_nodes))
+        print mpf_util.MsgUtil.red('Child node %s has not been added to the cluster.' % node)
         return
+
+    nodes_list.append(node)
+
+    node_with_port = ''.join([node,'[',port,']'])
+    new_nodes_with_ports = ','.join([string.rstrip(all_mpf_nodes,','),node_with_port,'']) # add trailing comma
 
     # If the WFM is running, update the env. variable being used by the WFM
     request = urllib2.Request(workflow_manager_url)
@@ -65,16 +74,13 @@ def add_node(node, port=None, all_mpf_nodes=None, workflow_manager_url=None):
     if  wfm_running:
         print "Detected that the Workflow Manager is running. Updating value of ALL_MPF_NODES used by the Workflow Manager."
 
-        node_with_port = ''.join([node,'[',port,']'])
-        new_nodes = ','.join([string.rstrip(all_mpf_nodes,','),node_with_port,'']) # add trailing comma
-
         endpoint_url = ''.join([string.rstrip(workflow_manager_url,'/'),'/rest/properties/all-mpf-nodes'])
 
         print 'Enter the credentials for a Workflow Manager administrator:'
         username = raw_input('Username: ')
         password = getpass.getpass('Password: ')
 
-        request = urllib2.Request(endpoint_url, data=new_nodes)
+        request = urllib2.Request(endpoint_url, data=new_nodes_with_ports)
         request.get_method = lambda: 'PUT'
         base64string = base64.b64encode('%s:%s' % (username, password))
         request.add_header('Authorization', 'Basic %s' % base64string)
@@ -82,13 +88,20 @@ def add_node(node, port=None, all_mpf_nodes=None, workflow_manager_url=None):
         try:
             urllib2.urlopen(request)
         except:
-            print mpf_util.MsgUtil.red('Problem connecting to: %s' % endpoint_url)
-            print mpf_util.MsgUtil.red('Child node: %s has not been added' % node)
+            print mpf_util.MsgUtil.red('Problem connecting to %s' % endpoint_url)
+            print mpf_util.MsgUtil.red('Child node %s has not been added to the cluster.' % node)
             raise
 
-    # TODO: modify system files on master
+    # Modify system files
+    updated_mpf_sh = update_mpf_sh(new_nodes_with_ports)
+    updated_ansible_hosts = update_ansible_hosts(nodes_list)
 
-    print mpf_util.MsgUtil.green('Child node: %s has been added' % node)
+    if not updated_mpf_sh or not updated_ansible_hosts:
+        print mpf_util.MsgUtil.yellow('Child node %s has not been completely added to the cluster. Manual steps required.' % node)
+    else:
+        print mpf_util.MsgUtil.green('Child node %s has been added to the cluster.' % node)
+        print mpf_util.MsgUtil.green('Add the node through the Nodes page of the Web UI to configure services.')
+        print mpf_util.MsgUtil.green('Run \"source /etc/profile.d/mpf.sh\" in any open terminal windows.')
 
 
 @argh.arg('node', help='hostname or IP address of child node to remove', action=mpf_util.VerifyHostnameOrIpAddress)
@@ -96,8 +109,91 @@ def add_node(node, port=None, all_mpf_nodes=None, workflow_manager_url=None):
           help='Url to Workflow Manager')
 def remove_node(node, workflow_manager_url=None):
     """ Removes a child node from the Workflow Manager """
-    pass # DEBUG
-    print mpf_util.MsgUtil.green('Child node: %s has been removed' % node)
+    # TODO: Implement me!
+    print mpf_util.MsgUtil.green('Child node %s has been removed.' % node)
+
+
+def update_mpf_sh(nodes_with_ports):
+    filename = '/home/mpf/Desktop/TMP/mpf-test.sh' # DEBUG: /etc/profile.d/mpf.sh
+    findstr = 'export ALL_MPF_NODES='
+
+    error = False
+
+    if not os.path.isfile(filename):
+        print mpf_util.MsgUtil.red('Error: Could not open ' + filename + '.')
+        error = True
+
+    if not error:
+        retcode = call(['grep', '-q', findstr, filename])
+        if retcode != 0:
+            print mpf_util.MsgUtil.red('Error: Could not find \"' + findstr + '\" in ' + filename + '.')
+            error = True
+        else:
+            retcode = call(['sudo', 'sed', '-i', 's/\\(' + findstr + '\\).*/\\1' + nodes_with_ports + '/', filename])
+            if retcode != 0:
+                print mpf_util.MsgUtil.red('Error: Could not update \"' + findstr + '\" in ' + filename + '.')
+                error = True
+            else:
+                print 'Updated \"' + findstr + '\" in ' + filename + '.'
+
+    if error:
+        print mpf_util.MsgUtil.red('Please manually update \"' + findstr + '\" in ' + filename + '.')
+
+    return not error
+
+
+def update_ansible_hosts(nodes_list):
+    filename = '/home/mpf/Desktop/TMP/hosts-test' # DEBUG: /etc/ansible/hosts
+    findstr = '[mpf-child]'
+
+    error = False
+
+    try:
+        curr_line_num = -1
+        start_line_num = -1
+        stop_line_num = -1
+
+        with open(filename, 'r') as file:
+            lines = file.readlines()
+
+            for line in lines:
+                stripped_line = line.strip()
+                curr_line_num += 1
+                if start_line_num == -1:
+                    if stripped_line == findstr:
+                        start_line_num = curr_line_num
+                else:
+                    if not stripped_line:
+                        stop_line_num = curr_line_num-1 # reached blank line
+                        break
+
+            if start_line_num == -1:
+                print mpf_util.MsgUtil.red('Error: Could not find \"' + findstr + '\" in ' + filename + '.')
+                error = True
+            else:
+                if stop_line_num == -1:
+                    stop_line_num = curr_line_num # reached end of file
+
+        if not error:
+            with open(filename, 'w') as file:
+                for i in range (0, len(lines)):
+                    if i < start_line_num or i > stop_line_num:
+                        file.write(lines[i])
+                    elif i == start_line_num:
+                        file.write(findstr + '\n')
+                        for node in nodes_list:
+                            file.write(node + '\n')
+
+            print 'Updated \"' + findstr + '\" in ' + filename + '.'
+
+    except IOError:
+        print mpf_util.MsgUtil.red('Error: Could not open ' + filename + '.')
+        error = True
+
+    if error:
+        print mpf_util.MsgUtil.red('Please manually update \"' + findstr + '\" in ' + filename + '.')
+
+    return not error
 
 
 COMMANDS = (add_node, remove_node)

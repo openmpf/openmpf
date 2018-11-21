aae3a392e70ac70b42f747a72ee92ba8199e8570
jrobble <jrobble@mitre.org>
2018-11-21 07:10:36 -0500

    Refactored.

--- trunk/workflow-manager/src/test/java/org/mitre/mpf/wfm/camel/TestDlqRouteBuilder.java
+++ trunk/workflow-manager/src/test/java/org/mitre/mpf/wfm/camel/TestDlqRouteBuilder.java
@@ -31,10 +31,7 @@ import org.apache.activemq.camel.component.ActiveMQComponent;
 import org.apache.camel.CamelContext;
 import org.apache.camel.component.jms.JmsComponent;
 import org.apache.camel.impl.DefaultCamelContext;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.FixMethodOrder;
-import org.junit.Test;
+import org.junit.*;
 import org.junit.runners.MethodSorters;
 import org.mitre.mpf.component.executor.detection.MPFDetectionMain;
 import org.mitre.mpf.wfm.camel.operations.detection.DetectionDeadLetterProcessor;
@@ -43,6 +40,8 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
 import javax.jms.*;
+
+import java.util.UUID;
 
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.*;
@@ -56,10 +55,17 @@ public class TestDlqRouteBuilder {
     public static final String ROUTE_ID = "Test DLQ Route";
     public static final String SELECTOR_REPLY_TO = "queue://MPF.TEST.COMPLETED_DETECTIONS";
 
-    public static final String DUPLICATE_FROM_STORE_CAUSE =
+    public static final String BAD_SELECTOR_REPLY_TO = "queue://MPF.TEST.BAD.COMPLETED_DETECTIONS";
+
+    public static final String DLQ_DELIVERY_FAILURE_CAUSE_PROPERTY = "dlqDeliveryFailureCause";
+    public static final String DLQ_DUPLICATE_FAILURE_CAUSE =
             "java.lang.Throwable: duplicate from store for queue://MPF.DETECTION_DUMMY_REQUEST";
+    public static final String DLQ_OTHER_FAILURE_CAUSE = "SOME OTHER FAILURE";
+
+    public static final String TEST_BODY = "THIS IS A TEST MESSAGE";
 
     public static final int SLEEP_TIME_MILLISEC = 500;
+    public static final int RECEIVE_TIMEOUT_MILLISEC = 500;
 
     private CamelContext camelContext;
     private ConnectionFactory connectionFactory;
@@ -71,7 +77,7 @@ public class TestDlqRouteBuilder {
     private DetectionDeadLetterProcessor mockDetectionDeadLetterProcessor;
 
     @Before
-    public void init() throws Exception {
+    public void setup() throws Exception {
         MockitoAnnotations.initMocks(this);
 
         camelContext = new DefaultCamelContext();
@@ -84,13 +90,17 @@ public class TestDlqRouteBuilder {
         connection.start();
 
         session = connection.createSession(true, Session.AUTO_ACKNOWLEDGE);
-        
+
         DlqRouteBuilder dlqRouteBuilder =
                 new DlqRouteBuilder(ENTRY_POINT, EXIT_POINT, AUDIT_EXIT_POINT, ROUTE_ID, SELECTOR_REPLY_TO, false);
 
         dlqRouteBuilder.setDeadLetterProcessor(mockDetectionDeadLetterProcessor);
         dlqRouteBuilder.setContext(camelContext);
         camelContext.addRoutes(dlqRouteBuilder);
+
+        clearoutMessages(ENTRY_POINT);
+        // clearoutMessages(EXIT_POINT);
+        // clearoutMessages(AUDIT_EXIT_POINT);
     }
 
     @After
@@ -107,62 +117,129 @@ public class TestDlqRouteBuilder {
         return fullName.replaceAll(".*//", "");
     }
 
-    private void sendDlqMessage(String dest, String replyTo, String deliveryFailureCause) throws JMSException {
-        ObjectMessage message = session.createObjectMessage();
-        message.setObject("TEST BODY");
+    private void clearoutMessages(String dest) throws JMSException {
+        Destination dlqDestination = session.createQueue(getJmsOrQueueShortName(dest));
+        MessageConsumer messageConsumer = session.createConsumer(dlqDestination);
 
-        if (deliveryFailureCause != null) {
-            message.setStringProperty("dlqDeliveryFailureCause", deliveryFailureCause);
+        Message message;
+        while ((message = messageConsumer.receive(RECEIVE_TIMEOUT_MILLISEC)) != null) { // don't wait
+            if (message.getJMSCorrelationID() != null) {
+                System.out.println("Clearing out message from " + dest + " with JMSCorrelationID " + message.getJMSCorrelationID());
+            } else {
+                System.out.println("Clearing out message from " + dest);
+            }
         }
 
-        Destination replyToDestination = session.createQueue(getJmsOrQueueShortName(replyTo));
-        message.setJMSReplyTo(replyToDestination);
+        session.commit();
+        messageConsumer.close();
+    }
+
+    private Message receiveMessage(String dest) throws JMSException {
+        Destination dlqDestination = session.createQueue(getJmsOrQueueShortName(dest));
+        MessageConsumer messageConsumer = session.createConsumer(dlqDestination);
+        Message message = messageConsumer.receive(RECEIVE_TIMEOUT_MILLISEC);
+        session.commit();
+        messageConsumer.close();
+        return message;
+    }
+
+    private void consumeLeftover(String dest, String replyTo, String deliveryFailureCause, String jmsCorrelationId)
+            throws JMSException {
+        Message message = receiveMessage(dest);
+        Assert.assertNotNull(message);
+
+        if (replyTo == null) {
+            Assert.assertNull(message.getJMSReplyTo());
+        } else {
+            Assert.assertEquals(replyTo, message.getJMSReplyTo().toString());
+        }
+
+        Assert.assertEquals(deliveryFailureCause, message.getStringProperty(DLQ_DELIVERY_FAILURE_CAUSE_PROPERTY));
+        Assert.assertEquals(TEST_BODY, ((ObjectMessage)message).getObject().toString());
+        Assert.assertEquals(jmsCorrelationId, message.getJMSCorrelationID());
+    }
+
+    private String sendDlqMessage(String dest, String replyTo, String deliveryFailureCause) throws JMSException {
+        ObjectMessage message = session.createObjectMessage();
+        message.setObject(TEST_BODY);
+
+        if (replyTo != null) {
+            Destination replyToDestination = session.createQueue(getJmsOrQueueShortName(replyTo));
+            message.setJMSReplyTo(replyToDestination);
+        }
+
+        if (deliveryFailureCause != null) {
+            message.setStringProperty(DLQ_DELIVERY_FAILURE_CAUSE_PROPERTY, deliveryFailureCause);
+        }
+
+        String jmsCorrelationId = UUID.randomUUID().toString();
+        message.setJMSCorrelationID(jmsCorrelationId);
 
         Destination dlqDestination = session.createQueue(getJmsOrQueueShortName(dest));
         MessageProducer messageProducer = session.createProducer(dlqDestination);
         messageProducer.send(message);
         session.commit();
+
+        return jmsCorrelationId;
+    }
+
+    private void runTest(String dest, String replyTo, String deliveryFailureCause, boolean isHandled) throws Exception {
+        String jmsCorrelationId = sendDlqMessage(dest, replyTo, deliveryFailureCause);
+        Thread.sleep(SLEEP_TIME_MILLISEC);
+        verify(mockDetectionDeadLetterProcessor, times(isHandled ? 1 : 0)).process(any());
+        if (!isHandled) {
+            consumeLeftover(dest, replyTo, deliveryFailureCause, jmsCorrelationId);
+        }
+    }
+
+    // No reply-to tests
+
+    @Test
+    public void ignoreNoReplyToAndDupFailure() throws Exception {
+        runTest(ENTRY_POINT, null, DLQ_DUPLICATE_FAILURE_CAUSE, false);
     }
 
     @Test
-    public void ignoreBadReplyToDupFailure() throws Exception {
-        sendDlqMessage(ENTRY_POINT, "BAD_REPLY_TO", DUPLICATE_FROM_STORE_CAUSE);
-        Thread.sleep(SLEEP_TIME_MILLISEC);
-        verify(mockDetectionDeadLetterProcessor, never()).process(any());
+    public void ignoreNoReplyToAndNoFailure() throws Exception {
+        runTest(ENTRY_POINT, null, null, false);
     }
 
     @Test
-    public void ignoreBadReplyToNoFailure() throws Exception {
-        sendDlqMessage(ENTRY_POINT, "BAD_REPLY_TO", null);
-        Thread.sleep(SLEEP_TIME_MILLISEC);
-        verify(mockDetectionDeadLetterProcessor, never()).process(any());
+    public void ignoreNoReplyToAndNonDupFailure() throws Exception {
+        runTest(ENTRY_POINT, null, DLQ_OTHER_FAILURE_CAUSE, false);
+    }
+
+    // Bad reply-to tests
+
+    @Test
+    public void ignoreBadReplyToAndDupFailure() throws Exception {
+        runTest(ENTRY_POINT, BAD_SELECTOR_REPLY_TO, DLQ_DUPLICATE_FAILURE_CAUSE, false);
     }
 
     @Test
-    public void ignoreBadReplyToNonDupFailure() throws Exception {
-        sendDlqMessage(ENTRY_POINT, "BAD_REPLY_TO", "OTHER FAILURE");
-        Thread.sleep(SLEEP_TIME_MILLISEC);
-        verify(mockDetectionDeadLetterProcessor, never()).process(any());
+    public void ignoreBadReplyToAndNoFailure() throws Exception {
+        runTest(ENTRY_POINT, BAD_SELECTOR_REPLY_TO, null, false);
     }
+
+    @Test
+    public void ignoreBadReplyToAndNonDupFailure() throws Exception {
+        runTest(ENTRY_POINT, BAD_SELECTOR_REPLY_TO, DLQ_OTHER_FAILURE_CAUSE, false);
+    }
+
+    // Good reply-to tests
 
     @Test
     public void ignoreDupFailure() throws Exception {
-        sendDlqMessage(ENTRY_POINT, SELECTOR_REPLY_TO, DUPLICATE_FROM_STORE_CAUSE);
-        Thread.sleep(SLEEP_TIME_MILLISEC);
-        verify(mockDetectionDeadLetterProcessor, never()).process(any());
-    }
-
-    @Test
-    public void handleNonDupFailure() throws Exception {
-        sendDlqMessage(ENTRY_POINT, SELECTOR_REPLY_TO, "OTHER FAILURE");
-        Thread.sleep(SLEEP_TIME_MILLISEC);
-        verify(mockDetectionDeadLetterProcessor, atLeastOnce()).process(any());
+        runTest(ENTRY_POINT, SELECTOR_REPLY_TO, DLQ_DUPLICATE_FAILURE_CAUSE, false);
     }
 
     @Test
     public void handleNoFailure() throws Exception {
-        sendDlqMessage(ENTRY_POINT, SELECTOR_REPLY_TO, null);
-        Thread.sleep(SLEEP_TIME_MILLISEC);
-        verify(mockDetectionDeadLetterProcessor, atLeastOnce()).process(any());
+        runTest(ENTRY_POINT, SELECTOR_REPLY_TO, null, true);
+    }
+
+    @Test
+    public void handleNonDupFailure() throws Exception {
+        runTest(ENTRY_POINT, SELECTOR_REPLY_TO, DLQ_OTHER_FAILURE_CAUSE, true);
     }
 }

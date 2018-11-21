0d21fade0fea40877bb297a51229480dab16b532
jrobble <jrobble@mitre.org>
2018-11-21 03:41:44 -0500

    WIP. Tests work with failure cause.

--- trunk/workflow-manager/src/main/java/org/mitre/mpf/wfm/camel/routes/DlqRouteBuilder.java
+++ trunk/workflow-manager/src/main/java/org/mitre/mpf/wfm/camel/routes/DlqRouteBuilder.java
@@ -30,12 +30,15 @@ import com.google.protobuf.InvalidProtocolBufferException;
 import org.apache.camel.ExchangePattern;
 import org.apache.camel.LoggingLevel;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.model.ProcessorDefinition;
 import org.mitre.mpf.wfm.buffers.DetectionProtobuf;
 import org.mitre.mpf.wfm.camel.operations.detection.DetectionDeadLetterProcessor;
 import org.mitre.mpf.wfm.enums.MpfEndpoints;
 import org.mitre.mpf.wfm.enums.MpfHeaders;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 /**
@@ -46,56 +49,76 @@ public class DlqRouteBuilder extends RouteBuilder {
 	private static final Logger log = LoggerFactory.getLogger(DlqRouteBuilder.class);
 
 	public static final String ENTRY_POINT = MpfEndpoints.DEAD_LETTER_QUEUE;
-	public static final String EXIT_POINT = MpfEndpoints.DLQ_PROCESSED_MESSAGES;
+	public static final String EXIT_POINT = MpfEndpoints.COMPLETED_DETECTIONS;
+	public static final String AUDIT_EXIT_POINT = MpfEndpoints.DLQ_PROCESSED_MESSAGES;
 	public static final String ROUTE_ID = "DLQ Route";
+	public static final String SELECTOR_REPLY_TO = MpfEndpoints.COMPLETED_DETECTIONS_REPLY_TO;
 
-	private final String entryPoint, exitPoint, routeId;
+	private String entryPoint, exitPoint, auditExitPoint, routeId, selectorReplyTo;
+	private boolean unmarshalProtobuf = true; // for testing purposes
+
+	@Autowired
+	@Qualifier(DetectionDeadLetterProcessor.REF)
+	private DetectionDeadLetterProcessor detectionDeadLetterProcessor;
 
 	public DlqRouteBuilder() {
-		this(ENTRY_POINT, EXIT_POINT, ROUTE_ID);
+		this(ENTRY_POINT, EXIT_POINT, AUDIT_EXIT_POINT, ROUTE_ID, SELECTOR_REPLY_TO, true);
 	}
 
-	public DlqRouteBuilder(String entryPoint, String exitPoint, String routeId) {
+	public DlqRouteBuilder(String entryPoint, String exitPoint, String auditExitPoint, String routeId,
+						   String selectorReplyTo, boolean unmarshalProtobuf) {
 		this.entryPoint = entryPoint;
 		this.exitPoint = exitPoint;
+		this.auditExitPoint = auditExitPoint;
 		this.routeId = routeId;
+		this.selectorReplyTo = selectorReplyTo;
+		this.unmarshalProtobuf = unmarshalProtobuf;
+	}
+
+	public void setDeadLetterProcessor(DetectionDeadLetterProcessor deadLetterProcessor) {
+		this.detectionDeadLetterProcessor = deadLetterProcessor;
 	}
 
 	@Override
 	public void configure() throws Exception {
 		log.debug("Configuring route '{}'.", routeId);
 
-		// Only process detection messages sent to components; otherwise leave messages on the default DLQ for auditing.
-		String selector = "?selector=" + java.net.URLEncoder.encode(MpfHeaders.JMS_REPLY_TO + "='" + MpfEndpoints.COMPLETED_DETECTIONS_REPLY_TO + "'","UTF-8");
+		// Only process detection messages sent to components and not duplicates; otherwise leave messages on the
+		// default DLQ for auditing.
+        String selector = "?selector=" + java.net.URLEncoder.encode(MpfHeaders.JMS_REPLY_TO + "='" + selectorReplyTo + "'"
+                + " AND (dlqDeliveryFailureCause IS NULL OR dlqDeliveryFailureCause NOT LIKE '%duplicate from store%')", "UTF-8");
 
-		from(entryPoint + selector)
+        ProcessorDefinition def = from(entryPoint + selector)
 			.routeId(routeId)
-			.setExchangePattern(ExchangePattern.InOnly)
+			.setExchangePattern(ExchangePattern.InOnly);
 
-				// Configure exception handler(s) before behaviors that can cause exceptions.
-				.onException(InvalidProtocolBufferException.class)
-					.log(LoggingLevel.ERROR, DlqRouteBuilder.class.getName(),
-						"Cannot parse message into a DetectionRequest. Cannot mark job ${header." + MpfHeaders.JOB_ID + "} as completed.")
-					// ActiveMQ DefaultErrorHandler will print out message details.
-					.to(MpfEndpoints.DLQ_INVALID_MESSAGES) // send possibly corrupted message to a separate queue for auditing
-					.handled(true) // prevent further camel processing
-				.end()
+				if (unmarshalProtobuf) {
+					// Configure exception handler(s) before behaviors that can cause exceptions.
+					def = def.onException(InvalidProtocolBufferException.class)
+						.log(LoggingLevel.ERROR, DlqRouteBuilder.class.getName(),
+							"Cannot parse message into a DetectionRequest. Cannot mark job ${header." + MpfHeaders.JOB_ID + "} as completed.")
+						// ActiveMQ DefaultErrorHandler will print out message details.
+						.to(MpfEndpoints.DLQ_INVALID_MESSAGES) // send possibly corrupted message to a separate queue for auditing
+						.handled(true) // prevent further camel processing
+					.end()
 
-				// Attempt to deserialize the protobuf message now. Failure to deserialize will trigger the exception
-				// handler and prevent further exceptions and redelivery attempts for this message.
+					// Attempt to deserialize the protobuf message now. Failure to deserialize will trigger the exception
+					// handler and prevent further exceptions and redelivery attempts for this message.
 
-				// On at least one occasion, the following exception was observed:
-				//     "com.google.protobuf.InvalidProtocolBufferException: Message missing required fields: data_uri"
-				// Further debugging is necessary to determine the root cause.
+					// On at least one occasion, the following exception was observed:
+					//     "com.google.protobuf.InvalidProtocolBufferException: Message missing required fields: data_uri"
+					// Further debugging is necessary to determine the root cause.
 
-				.unmarshal().protobuf(DetectionProtobuf.DetectionRequest.getDefaultInstance()).convertBodyTo(String.class)
-				.multicast()
+					.unmarshal().protobuf(DetectionProtobuf.DetectionRequest.getDefaultInstance()).convertBodyTo(String.class);
+				}
+
+				def.multicast()
 					.pipeline()
-						.to(exitPoint) // send deserialized (readable) message to the exit point to indicate it has been processed, and for auditing
+						.to(auditExitPoint) // send deserialized (readable) message to the exit point to indicate it has been processed, and for auditing
 					.end()
 					.pipeline()
-						.process(DetectionDeadLetterProcessor.REF) // generate a detection response protobuf message with an error status
-						.to(MpfEndpoints.COMPLETED_DETECTIONS) // send protobuf message to the intended destination to increment the job count
+						.process(detectionDeadLetterProcessor) // generate a detection response protobuf message with an error status
+						.to(exitPoint) // send protobuf message to the intended destination to increment the job count
 					.end()
 				.end()
 			.end();
--- trunk/workflow-manager/src/test/java/org/mitre/mpf/wfm/camel/TestDlqRouteBuilder.java
+++ trunk/workflow-manager/src/test/java/org/mitre/mpf/wfm/camel/TestDlqRouteBuilder.java
@@ -26,53 +26,79 @@
 
 package org.mitre.mpf.wfm.camel;
 
-import com.uwyn.jhighlight.tools.StringUtils;
 import org.apache.activemq.ActiveMQConnectionFactory;
 import org.apache.activemq.camel.component.ActiveMQComponent;
 import org.apache.camel.CamelContext;
+import org.apache.camel.Exchange;
+import org.apache.camel.ExchangePattern;
+import org.apache.camel.ProducerTemplate;
 import org.apache.camel.component.jms.JmsComponent;
 import org.apache.camel.impl.DefaultCamelContext;
+import org.apache.camel.impl.DefaultExchange;
+import org.apache.camel.impl.DefaultMessage;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
+import org.junit.runners.MethodSorters;
 import org.mitre.mpf.component.executor.detection.MPFDetectionMain;
 import org.mitre.mpf.wfm.buffers.DetectionProtobuf;
 import org.mitre.mpf.wfm.camel.operations.detection.DetectionDeadLetterProcessor;
 import org.mitre.mpf.wfm.camel.routes.DlqRouteBuilder;
-import org.mitre.mpf.wfm.enums.MpfEndpoints;
+import org.mitre.mpf.wfm.enums.MpfHeaders;
 import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
 import javax.jms.*;
 
-//@ContextConfiguration(locations = {"classpath:applicationContext.xml"})
-//@RunWith(SpringJUnit4ClassRunner.class)
-//@RunListener.ThreadSafe
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.*;
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class TestDlqRouteBuilder {
 
-    private CamelContext camelContext = new DefaultCamelContext();
+    private CamelContext camelContext;
 
-    private ConnectionFactory connectionFactory = new ActiveMQConnectionFactory(MPFDetectionMain.ACTIVEMQHOST);
+    private ConnectionFactory connectionFactory;
 
-    @Mock
-    private DlqRouteBuilder mockDlqRouteBuilder;
+    private JmsComponent activeMQComponent;
+
+    public static final String ENTRY_POINT = "jms://MPF.TEST.ActiveMQ.DLQ";
+    public static final String EXIT_POINT = "jms:MPF.TEST.COMPLETED_DETECTIONS";
+    public static final String AUDIT_EXIT_POINT = "jms://MPF.TEST.DLQ_PROCESSED_MESSAGES";
+    public static final String ROUTE_ID = "Test DLQ Route";
+    public static final String SELECTOR_REPLY_TO = "queue://MPF.TEST.COMPLETED_DETECTIONS";
+
+    public static final String DUPLICATE_FROM_STORE_CAUSE =
+            "java.lang.Throwable: duplicate from store for queue://MPF.DETECTION_DUMMY_REQUEST";
+
+    public static final int SLEEP_TIME_MILLISEC = 500;
 
     @Mock
     private DetectionDeadLetterProcessor mockDetectionDeadLetterProcessor;
 
     @Before
     public void init() throws Exception {
-        // MockitoAnnotations.initMocks(this);
+        // System.setProperty("org.apache.activemq.SERIALIZABLE_PACKAGES", "com.google.protobuf");
+
+        MockitoAnnotations.initMocks(this);
 
         camelContext = new DefaultCamelContext();
-        JmsComponent activeMQComponent = ActiveMQComponent.jmsComponentAutoAcknowledge(connectionFactory);
+        connectionFactory = new ActiveMQConnectionFactory(MPFDetectionMain.ACTIVEMQHOST);
+        activeMQComponent = ActiveMQComponent.jmsComponentAutoAcknowledge(connectionFactory);
         camelContext.addComponent("jms", activeMQComponent);
 
+        // camelContext.getTypeConverterRegistry().addTypeConverter(java.io.InputStream.class,
+        //        org.mitre.mpf.wfm.buffers.DetectionProtobuf.DetectionRequest.class, new DetectionRequestConverter());
 
-        DlqRouteBuilder dlqRouteBuilder = new DlqRouteBuilder();
-        dlqRouteBuilder.setD(new DetectionDeadLetterProcessor()); // TODO
+        DlqRouteBuilder dlqRouteBuilder =
+                new DlqRouteBuilder(ENTRY_POINT, EXIT_POINT, AUDIT_EXIT_POINT, ROUTE_ID, SELECTOR_REPLY_TO, false);
+        // dlqRouteBuilder.setDeadLetterProcessor(new DetectionDeadLetterProcessor()); // TODO
+        dlqRouteBuilder.setDeadLetterProcessor(mockDetectionDeadLetterProcessor);
         dlqRouteBuilder.setContext(camelContext);
-        camelContext.addRoutes(dlqRouteBuilder); // mockDlqRouteBuilder
+        camelContext.addRoutes(dlqRouteBuilder);
 
+        // camelContext.getTypeConverter().
 
         camelContext.start();
     }
@@ -84,36 +110,155 @@ public class TestDlqRouteBuilder {
         }
     }
 
-    private void sendDlqMessage() throws JMSException {
+    private void sendDlqMessageBack(String dest, String replyTo, boolean withDeliveryFailureCause) throws JMSException {
+        //Connection connection = null;
+        try {
+            //connection = connectionFactory.createConnection();
+            //connection.start();
+            //Session session = connection.createSession(true, Session.AUTO_ACKNOWLEDGE);
+
+            // BytesMessage message = session.createBytesMessage();
+            // message.writeBytes(DetectionProtobuf.DetectionRequest.getDefaultInstance().toByteArray());
+
+            activeMQComponent.getConfiguration().setReplyToOverride(replyTo);
+
+            DefaultMessage message = new DefaultMessage();
+            message.setBody(DetectionProtobuf.DetectionRequest.getDefaultInstance());
+            message.setHeader(MpfHeaders.JMS_REPLY_TO, replyTo); // NOTE: This does nothing when using ExchangePattern.InOnly
+
+            if (withDeliveryFailureCause) {
+                message.setHeader("dlqDeliveryFailureCause",
+                        "java.lang.Throwable: duplicate from store for queue://MPF.DETECTION_DUMMY_REQUEST");
+            }
+
+            // Destination replyToDestination = session.createQueue(replyTo);
+            // message.setJMSReplyTo(replyToDestination);
+
+            Exchange exchange = new DefaultExchange(camelContext);
+            exchange.setProperty(MpfHeaders.JMS_REPLY_TO, replyTo);
+            exchange.setPattern(ExchangePattern.OutOnly); // InOnly
+            exchange.setIn(message);
+            ProducerTemplate producerTemplate = camelContext.createProducerTemplate();
+            // producerTemplate.setDefaultEndpointUri(dest);
+            // producerTemplate.sendBodyAndHeader(DetectionProtobuf.DetectionRequest.getDefaultInstance(), MpfHeaders.JOB_ID, replyTo);
+            producerTemplate.send(dest, exchange);
+
+            // Destination dlq = session.createQueue("DUMMY"); //StringUtils.replace(MpfEndpoints.DEAD_LETTER_QUEUE, "activemq:", ""));
+            //Destination dlqDestination = session.createQueue(StringUtils.replace(MpfEndpoints.DEAD_LETTER_QUEUE, "activemq:", ""));
+
+            //MessageProducer messageProducer = session.createProducer(dlqDestination);
+            //messageProducer.send(message);
+            //session.commit();
+        } finally {
+            //if (connection != null) {
+            //    connection.close();
+            //}
+        }
+    }
+
+    private String getJmsOrQueueShortName(String fullName) {
+        return fullName.replaceAll(".*//", "");
+    }
+
+    private void sendDlqMessage(String dest, String replyTo, String deliveryFailureCause) throws JMSException {
         Connection connection = null;
         try {
             connection = connectionFactory.createConnection();
             connection.start();
             Session session = connection.createSession(true, Session.AUTO_ACKNOWLEDGE);
 
-            BytesMessage message = session.createBytesMessage();
-            message.writeBytes(DetectionProtobuf.DetectionRequest.getDefaultInstance().toByteArray());
-            Destination replyTo = session.createQueue(StringUtils.replace(MpfEndpoints.COMPLETED_DETECTIONS, "jms:", ""));
-            message.setJMSReplyTo(replyTo);
-            // message.setStringProperty("dlqDeliveryFailureCause", "java.lang.Throwable: duplicate from store for queue://MPF.DETECTION_DUMMY_REQUEST");
+            // BytesMessage message = session.createBytesMessage();
+            // message.writeBytes(DetectionProtobuf.DetectionRequest.getDefaultInstance().toByteArray());
 
-            // Destination dlq = session.createQueue("DUMMY"); //StringUtils.replace(MpfEndpoints.DEAD_LETTER_QUEUE, "activemq:", ""));
-            Destination dlq = session.createQueue(StringUtils.replace(MpfEndpoints.DEAD_LETTER_QUEUE, "activemq:", ""));
+            ObjectMessage message = session.createObjectMessage();
+            // message.setObject(DetectionProtobuf.DetectionRequest.getDefaultInstance());
+            message.setObject("TEST BODY");
 
-            MessageProducer messageProducer = session.createProducer(dlq);
+            if (deliveryFailureCause != null) {
+                message.setStringProperty("dlqDeliveryFailureCause", deliveryFailureCause);
+            }
+
+            Destination replyToDestination = session.createQueue(getJmsOrQueueShortName(replyTo));
+            message.setJMSReplyTo(replyToDestination);
+
+            Destination dlqDestination = session.createQueue(getJmsOrQueueShortName(dest));
+            MessageProducer messageProducer = session.createProducer(dlqDestination);
             messageProducer.send(message);
             session.commit();
+
+            session.
         } finally {
             if (connection != null) {
-                connection.close();
+               connection.close();
             }
         }
     }
 
     @Test
-    public void testHandleDuplicateMessage() throws Exception {
+    public void testIgnoreBadReplyToDupFailure() throws Exception {
+        sendDlqMessage(ENTRY_POINT, "BAD_REPLY_TO", DUPLICATE_FROM_STORE_CAUSE);
+        Thread.sleep(SLEEP_TIME_MILLISEC);
+        verify(mockDetectionDeadLetterProcessor, never()).process(any());
+    }
 
-        sendDlqMessage();
+    @Test
+    public void testIgnoreBadReplyToNoFailure() throws Exception {
+        sendDlqMessage(ENTRY_POINT, "BAD_REPLY_TO", null);
+        Thread.sleep(SLEEP_TIME_MILLISEC);
+        verify(mockDetectionDeadLetterProcessor, never()).process(any());
+    }
+
+    @Test
+    public void testIgnoreBadReplyToNonDupFailure() throws Exception {
+        sendDlqMessage(ENTRY_POINT, "BAD_REPLY_TO", "OTHER FAILURE");
+        Thread.sleep(SLEEP_TIME_MILLISEC);
+        verify(mockDetectionDeadLetterProcessor, never()).process(any());
+    }
+
+    @Test
+    public void testIgnoreDupFailure() throws Exception {
+        sendDlqMessage(ENTRY_POINT, SELECTOR_REPLY_TO, DUPLICATE_FROM_STORE_CAUSE);
+        Thread.sleep(SLEEP_TIME_MILLISEC);
+        verify(mockDetectionDeadLetterProcessor, never()).process(any());
+    }
+
+    @Test
+    public void testHandleNonDupFailure() throws Exception {
+        sendDlqMessage(ENTRY_POINT, SELECTOR_REPLY_TO, "OTHER FAILURE");
+        Thread.sleep(SLEEP_TIME_MILLISEC);
+        verify(mockDetectionDeadLetterProcessor, atLeastOnce()).process(any());
+    }
+
+    @Test
+    public void testHandleNoFailure() throws Exception {
+        sendDlqMessage(ENTRY_POINT, SELECTOR_REPLY_TO, null);
+        Thread.sleep(SLEEP_TIME_MILLISEC);
+        verify(mockDetectionDeadLetterProcessor, atLeastOnce()).process(any());
+
+
+        // NOTE: reply-to not populated
+        //sendDlqMessage(StringUtils.replace(MpfEndpoints.DEAD_LETTER_QUEUE, "activemq:", ""),
+        //        StringUtils.replace(MpfEndpoints.COMPLETED_DETECTIONS, "jms:", ""), true);
+        //Thread.sleep(5000);
+        //verify(mockDetectionDeadLetterProcessor, atLeastOnce()).process(any());
+
+        // NOTE: reply-to populated
+        //sendDlqMessage("jms://DUMMY", MpfEndpoints.COMPLETED_DETECTIONS_REPLY_TO, true); // MpfEndpoints.DEAD_LETTER_QUEUE
+        //Thread.sleep(5000);
+        //verify(mockDetectionDeadLetterProcessor, atLeastOnce()).process(any());
+
+
+        // NOTE: reply-to not populated
+        //sendDlqMessageBack(MpfEndpoints.DEAD_LETTER_QUEUE, MpfEndpoints.COMPLETED_DETECTIONS_REPLY_TO, true);
+        //Thread.sleep(5000);
+        //verify(mockDetectionDeadLetterProcessor, atLeastOnce()).process(any());
+
+        // NOTE: reply-to populated
+        //sendDlqMessageBack("jms://DUMMY", MpfEndpoints.COMPLETED_DETECTIONS_REPLY_TO, true); // MpfEndpoints.DEAD_LETTER_QUEUE
+        //Thread.sleep(5000);
+        //verify(mockDetectionDeadLetterProcessor, atLeastOnce()).process(any());
+
+
 
         //Exchange exchange = new DefaultExchange(camelContext);
         //exchange.setPattern(ExchangePattern.InOnly);

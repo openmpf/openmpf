3e761047d2dd84adfa182e52e93ab38dcb2e5c00
Jeff Robble <fejjro@gmail.com>
2019-01-14 13:30:53 -0500

    Improved frame extraction error handling. (#801)
    * Refactored JNI unit tests.

--- trunk/video-overlay/src/test/java/org/mitre/mpf/frameextractor/TestFrameExtractor.java
+++ trunk/video-overlay/src/test/java/org/mitre/mpf/frameextractor/TestFrameExtractor.java
@@ -30,74 +30,55 @@ import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.mitre.mpf.JniTestUtils;
 
-import java.io.File;
 import java.io.IOException;
-import java.net.URISyntaxException;
-import java.net.URL;
+import java.net.URI;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Stream;
 
-import static java.util.stream.Collectors.toList;
+import static java.util.stream.Collectors.toSet;
 
 public class TestFrameExtractor {
-    private static final Logger log = LoggerFactory.getLogger(TestFrameExtractor.class);
-
     static {
-        String libraryFile = new File("install/lib/libmpfopencvjni.so").getAbsolutePath();
-        try {
-            System.load(libraryFile);
-        }
-        catch (UnsatisfiedLinkError e) {
-            String mpfHome = System.getenv("MPF_HOME");
-            if (mpfHome == null) {
-                throw e;
-            }
-            libraryFile = new File(mpfHome, "lib/libmpfopencvjni.so").getAbsolutePath();
-            System.load(libraryFile);
-        }
-        log.info("Loaded libmpfopencvjni library from '{}'.", libraryFile);
+        Assert.assertTrue(JniTestUtils.jniLibsLoaded());
     }
+
 
     @Rule
     public TemporaryFolder tempFolder = new TemporaryFolder();
 
 
     @Test
-    public void testFrameExtractorOnVideo() throws IOException, URISyntaxException {
-        extractFrames("samples/five-second-marathon-clip.mkv");
+    public void testFrameExtractorOnVideo() throws IOException {
+        extractFrames("samples/five-second-marathon-clip-numbered.mp4",
+                Arrays.asList(0, 1, 2, 4, 8, 100, 1000, 1500, 1998, 1999));
     }
 
     @Test
-    public void testFrameExtractorOnGif() throws URISyntaxException, IOException {
-        extractFrames("samples/face-morphing.gif");
+    public void testFrameExtractorOnGif() throws IOException {
+        extractFrames("samples/face-morphing.gif", Arrays.asList(2, 4, 8));
     }
 
-
-
-    private void extractFrames(String resourcePath) throws IOException, URISyntaxException {
-        URL resource = TestFrameExtractor.class.getClassLoader().getResource(resourcePath);
-        Assert.assertNotNull(resource);
+    private void extractFrames(String resourcePath, List<Integer> frames) throws IOException {
+        URI resource = JniTestUtils.getFileResource(resourcePath);
 
         Path outputDirectory = tempFolder.newFolder().toPath().toAbsolutePath();
-
-        FrameExtractor extractor = new FrameExtractor(resource.toURI(), outputDirectory.toUri());
-        extractor.getFrames().add(2);
-        extractor.getFrames().add(4);
-        extractor.getFrames().add(8);
+        FrameExtractor extractor = new FrameExtractor(resource, outputDirectory.toUri());
+        extractor.getFrames().addAll(frames);
         extractor.execute();
 
-        List<String> fileNames;
+        Set<String> fileNames;
         try(Stream<Path> paths = Files.list(outputDirectory)) {
             fileNames = paths.map(p -> p.getFileName().toString())
-                    .collect(toList());
+                    .collect(toSet());
         }
-        Assert.assertEquals(3, fileNames.size());
-        Assert.assertTrue(fileNames.containsAll(Arrays.asList("frame-2.png", "frame-4.png", "frame-8.png")));
+
+        Assert.assertEquals(frames.size(), fileNames.size());
+        frames.forEach(f -> Assert.assertTrue(fileNames.contains("frame-" + f + ".png")));
     }
 }

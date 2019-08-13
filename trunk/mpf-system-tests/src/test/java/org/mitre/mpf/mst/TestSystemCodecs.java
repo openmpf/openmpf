6087bf2d250444ddf1647d174cb43a87ccee92e0
Bryan Quinn <blq9064@rit.edu>
2019-08-13 15:37:15 -0400

    Updates based on review

--- trunk/mpf-system-tests/src/test/java/org/mitre/mpf/mst/TestSystemCodecs.java
+++ trunk/mpf-system-tests/src/test/java/org/mitre/mpf/mst/TestSystemCodecs.java
@@ -36,219 +36,107 @@ import java.util.List;
 
 public class TestSystemCodecs extends TestSystemWithDefaultConfig {
 
+    private boolean testHelper(String pipelineName, String testMediaPath) {
+        List<JsonMediaInputObject> media = toMediaObjectList(ioUtils.findFile(testMediaPath));
+
+        long jobId = runPipelineOnMedia(pipelineName, media);
+        JsonOutputObject outputObject = getJobOutputObject(jobId);
+
+        boolean detectionFound = outputObject.getMedia()
+                .stream()
+                .flatMap(m -> m.getTypes().values().stream())
+                .flatMap(Collection::stream)
+                .flatMap(a -> a.getTracks().stream())
+                .flatMap(t -> t.getDetections().stream())
+                .findAny()
+                .isPresent();
+
+        if (!detectionFound && outputObject.getJobErrors() != null) {
+            System.out.println(outputObject.getStatus());
+            System.err.println(outputObject.getJobErrors());
+            System.out.println(outputObject.getMedia());
+        }
+        return detectionFound;
+    }
+
     @Test(timeout = 5 * MINUTES)
-    public void runFaceOcvDetectImageAMR() {
+    public void runSpeechSphinxDetectAudioAMR() {
         String pipelineName = "SPHINX SPEECH DETECTION PIPELINE";
-        addPipeline(pipelineName);
+        String testMediaPath = "/samples/speech/amrnb.amr";
 
-        List<JsonMediaInputObject> media = toMediaObjectList(ioUtils.findFile("/samples/speech/amrnb.amr"));
-
-        long jobId = runPipelineOnMedia(pipelineName, media);
-        JsonOutputObject outputObject = getJobOutputObject(jobId);
-
-        boolean detectionFound = outputObject.getMedia()
-                .stream()
-                .flatMap(m -> m.getTypes().values().stream())
-                .flatMap(Collection::stream)
-                .flatMap(a -> a.getTracks().stream())
-                .flatMap(t -> t.getDetections().stream())
-                .findAny()
-                .isPresent();
-
-        if (!detectionFound && outputObject.getJobErrors() != null) {
-            System.out.println(outputObject.getStatus());
-            System.err.println(outputObject.getJobErrors());
-            System.out.println(outputObject.getMedia());
-        }
+        boolean detectionFound = testHelper(pipelineName, testMediaPath);
         Assert.assertTrue(detectionFound);
     }
 
     @Test(timeout = 5 * MINUTES)
-    public void runFaceOcvDetectImageH264AAC() {
+    public void runFaceOcvDetectVideoH264AAC() {
         String pipelineName = "OCV FACE DETECTION PIPELINE";
-        addPipeline(pipelineName);
+        String testMediaPath = "/samples/face/h264_aac.mp4";
 
-        List<JsonMediaInputObject> media = toMediaObjectList(ioUtils.findFile("/samples/face/h264_aac.mp4"));
-
-        long jobId = runPipelineOnMedia(pipelineName, media);
-        JsonOutputObject outputObject = getJobOutputObject(jobId);
-
-        boolean detectionFound = outputObject.getMedia()
-                .stream()
-                .flatMap(m -> m.getTypes().values().stream())
-                .flatMap(Collection::stream)
-                .flatMap(a -> a.getTracks().stream())
-                .flatMap(t -> t.getDetections().stream())
-                .findAny()
-                .isPresent();
-
-        if (!detectionFound && outputObject.getJobErrors() != null) {
-            System.out.println(outputObject.getStatus());
-            System.err.println(outputObject.getJobErrors());
-            System.out.println(outputObject.getMedia());
-        }
+        boolean detectionFound = testHelper(pipelineName, testMediaPath);
         Assert.assertTrue(detectionFound);
     }
 
     @Test(timeout = 5 * MINUTES)
-    public void runFaceOcvDetectImageH264MP3() {
+    public void runFaceOcvDetectVideoH264MP3() {
         String pipelineName = "OCV FACE DETECTION PIPELINE";
-        addPipeline(pipelineName);
+        String testMediaPath = "/samples/face/h264_mp3.mp4";
 
-        List<JsonMediaInputObject> media = toMediaObjectList(ioUtils.findFile("/samples/face/h264_mp3.mp4"));
-
-        long jobId = runPipelineOnMedia(pipelineName, media);
-        JsonOutputObject outputObject = getJobOutputObject(jobId);
-
-        boolean detectionFound = outputObject.getMedia()
-                .stream()
-                .flatMap(m -> m.getTypes().values().stream())
-                .flatMap(Collection::stream)
-                .flatMap(a -> a.getTracks().stream())
-                .flatMap(t -> t.getDetections().stream())
-                .findAny()
-                .isPresent();
-
-        if (!detectionFound && outputObject.getJobErrors() != null) {
-            System.out.println(outputObject.getStatus());
-            System.err.println(outputObject.getJobErrors());
-            System.out.println(outputObject.getMedia());
-        }
+        boolean detectionFound = testHelper(pipelineName, testMediaPath);
         Assert.assertTrue(detectionFound);
     }
 
     @Test(timeout = 5 * MINUTES)
-    public void runFaceOcvDetectImageH265AAC() {
+    public void runFaceOcvDetectVideoH265AAC() {
         String pipelineName = "OCV FACE DETECTION PIPELINE";
-        addPipeline(pipelineName);
+        String testMediaPath = "/samples/face/h265_aac.mp4";
 
-        List<JsonMediaInputObject> media = toMediaObjectList(ioUtils.findFile("/samples/face/h265_aac.mp4"));
-
-        long jobId = runPipelineOnMedia(pipelineName, media);
-        JsonOutputObject outputObject = getJobOutputObject(jobId);
-
-        boolean detectionFound = outputObject.getMedia()
-                .stream()
-                .flatMap(m -> m.getTypes().values().stream())
-                .flatMap(Collection::stream)
-                .flatMap(a -> a.getTracks().stream())
-                .flatMap(t -> t.getDetections().stream())
-                .findAny()
-                .isPresent();
-
-        if (!detectionFound && outputObject.getJobErrors() != null) {
-            System.out.println(outputObject.getStatus());
-            System.err.println(outputObject.getJobErrors());
-            System.out.println(outputObject.getMedia());
-        }
+        boolean detectionFound = testHelper(pipelineName, testMediaPath);
         Assert.assertTrue(detectionFound);
     }
 
     @Test(timeout = 5 * MINUTES)
-    public void runFaceOcvDetectImageXvidAAC() {
+    public void runFaceOcvDetectVideoMPv4AAC() {
         String pipelineName = "OCV FACE DETECTION PIPELINE";
-        addPipeline(pipelineName);
+        String testMediaPath = "/samples/face/mp4v_aac.mp4";
 
-        List<JsonMediaInputObject> media = toMediaObjectList(ioUtils.findFile("/samples/face/xvid_aac.mp4"));
-
-        long jobId = runPipelineOnMedia(pipelineName, media);
-        JsonOutputObject outputObject = getJobOutputObject(jobId);
-
-        boolean detectionFound = outputObject.getMedia()
-                .stream()
-                .flatMap(m -> m.getTypes().values().stream())
-                .flatMap(Collection::stream)
-                .flatMap(a -> a.getTracks().stream())
-                .flatMap(t -> t.getDetections().stream())
-                .findAny()
-                .isPresent();
-
-        if (!detectionFound && outputObject.getJobErrors() != null) {
-            System.out.println(outputObject.getStatus());
-            System.err.println(outputObject.getJobErrors());
-            System.out.println(outputObject.getMedia());
-        }
+        boolean detectionFound = testHelper(pipelineName, testMediaPath);
         Assert.assertTrue(detectionFound);
     }
 
     @Test(timeout = 5 * MINUTES)
-    public void runFaceOcvDetectImageTheoraOpus() {
+    public void runFaceOcvDetectVideoXvidAAC() {
         String pipelineName = "OCV FACE DETECTION PIPELINE";
-        addPipeline(pipelineName);
+        String testMediaPath = "/samples/face/xvid_aac.avi";
 
-        List<JsonMediaInputObject> media = toMediaObjectList(ioUtils.findFile("/samples/face/theora_opus.ogg"));
-
-        long jobId = runPipelineOnMedia(pipelineName, media);
-        JsonOutputObject outputObject = getJobOutputObject(jobId);
-
-        boolean detectionFound = outputObject.getMedia()
-                .stream()
-                .flatMap(m -> m.getTypes().values().stream())
-                .flatMap(Collection::stream)
-                .flatMap(a -> a.getTracks().stream())
-                .flatMap(t -> t.getDetections().stream())
-                .findAny()
-                .isPresent();
-
-        if (!detectionFound && outputObject.getJobErrors() != null) {
-            System.out.println(outputObject.getStatus());
-            System.err.println(outputObject.getJobErrors());
-            System.out.println(outputObject.getMedia());
-        }
+        boolean detectionFound = testHelper(pipelineName, testMediaPath);
         Assert.assertTrue(detectionFound);
     }
 
     @Test(timeout = 5 * MINUTES)
-    public void runFaceOcvDetectImageTheoraVorbis() {
+    public void runFaceOcvDetectVideoTheoraOpus() {
         String pipelineName = "OCV FACE DETECTION PIPELINE";
-        addPipeline(pipelineName);
+        String testMediaPath = "/samples/face/theora_opus.ogg";
 
-        List<JsonMediaInputObject> media = toMediaObjectList(ioUtils.findFile("/samples/face/theora_vorbis.ogg"));
-
-        long jobId = runPipelineOnMedia(pipelineName, media);
-        JsonOutputObject outputObject = getJobOutputObject(jobId);
-
-        boolean detectionFound = outputObject.getMedia()
-                .stream()
-                .flatMap(m -> m.getTypes().values().stream())
-                .flatMap(Collection::stream)
-                .flatMap(a -> a.getTracks().stream())
-                .flatMap(t -> t.getDetections().stream())
-                .findAny()
-                .isPresent();
-
-        if (!detectionFound && outputObject.getJobErrors() != null) {
-            System.out.println(outputObject.getStatus());
-            System.err.println(outputObject.getJobErrors());
-            System.out.println(outputObject.getMedia());
-        }
+        boolean detectionFound = testHelper(pipelineName, testMediaPath);
         Assert.assertTrue(detectionFound);
     }
 
     @Test(timeout = 5 * MINUTES)
-    public void runFaceOcvDetectImageVp8Opus() {
+    public void runFaceOcvDetectVideoTheoraVorbis() {
         String pipelineName = "OCV FACE DETECTION PIPELINE";
-        addPipeline(pipelineName);
+        String testMediaPath = "/samples/face/theora_vorbis.ogg";
 
-        List<JsonMediaInputObject> media = toMediaObjectList(ioUtils.findFile("/samples/face/vp8_opus.webm"));
+        boolean detectionFound = testHelper(pipelineName, testMediaPath);
+        Assert.assertTrue(detectionFound);
+    }
 
-        long jobId = runPipelineOnMedia(pipelineName, media);
-        JsonOutputObject outputObject = getJobOutputObject(jobId);
+    @Test(timeout = 5 * MINUTES)
+    public void runFaceOcvDetectVideoVp8Opus() {
+        String pipelineName = "OCV FACE DETECTION PIPELINE";
+        String testMediaPath = "/samples/face/vp8_opus.webm";
 
-        boolean detectionFound = outputObject.getMedia()
-                .stream()
-                .flatMap(m -> m.getTypes().values().stream())
-                .flatMap(Collection::stream)
-                .flatMap(a -> a.getTracks().stream())
-                .flatMap(t -> t.getDetections().stream())
-                .findAny()
-                .isPresent();
-
-        if (!detectionFound && outputObject.getJobErrors() != null) {
-            System.out.println(outputObject.getStatus());
-            System.err.println(outputObject.getJobErrors());
-            System.out.println(outputObject.getMedia());
-        }
+        boolean detectionFound = testHelper(pipelineName, testMediaPath);
         Assert.assertTrue(detectionFound);
     }
 

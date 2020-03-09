bf0c87a36ec2c1c0fac408d8e360fe10376cb1fc
Carol Nowacki <cnowacki@mitre.org>
2020-03-09 11:25:50 -0400

    forgot to add the frame extractor tests

--- trunk/video-overlay/src/test/java/org/mitre/mpf/frameextractor/TestFrameExtractor.java
+++ trunk/video-overlay/src/test/java/org/mitre/mpf/frameextractor/TestFrameExtractor.java
@@ -27,19 +27,35 @@
 package org.mitre.mpf.frameextractor;
 
 import org.junit.Assert;
+import static org.junit.Assert.assertTrue;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
 import org.mitre.mpf.JniTestUtils;
+import org.mitre.mpf.interop.JsonDetectionOutputObject;
+import org.mitre.mpf.interop.JsonTrackOutputObject;
 
+import com.google.common.collect.Table;
+
+import java.io.File;
 import java.io.IOException;
 import java.net.URI;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
+import java.util.SortedSet;
+import java.util.TreeSet;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
+
+import javax.imageio.ImageIO;
+import javax.imageio.ImageReader;
+import javax.imageio.stream.ImageInputStream;
 
 import static java.util.stream.Collectors.toSet;
 
@@ -55,30 +71,98 @@ public class TestFrameExtractor {
 
     @Test
     public void testFrameExtractorOnVideo() throws IOException {
-        extractFrames("samples/five-second-marathon-clip-numbered.mp4",
-                Arrays.asList(0, 1, 2, 4, 8, 100, 1000, 1500, 1998, 1999));
+        JsonTrackOutputObject track = createTrackObject(1, Arrays.asList(0, 1, 2, 4, 8, 100, 1000, 1500, 1998, 1999),
+                                                        100, 150);
+        extractFrames("samples/five-second-marathon-clip-numbered.mp4", Arrays.asList(track));
     }
 
     @Test
     public void testFrameExtractorOnGif() throws IOException {
-        extractFrames("samples/face-morphing.gif", Arrays.asList(2, 4, 8));
+        JsonTrackOutputObject track = createTrackObject(2, Arrays.asList(2, 4, 8),
+                                                        150, 100);
+        extractFrames("samples/face-morphing.gif", Arrays.asList(track));
     }
 
-    private void extractFrames(String resourcePath, List<Integer> frames) throws IOException {
+    @Test
+    public void testFrameExtractorOnImage() throws IOException {
+        JsonTrackOutputObject track = createTrackObject(3, Arrays.asList(0),
+                                                        400, 300);
+        extractFrames("samples/person_cropped_2.png", Arrays.asList(track));
+    }
+
+    @Test
+    public void testFrameExtractorMultipleTracks() throws IOException {
+        List<JsonTrackOutputObject> tracks = new ArrayList<>();
+        tracks.add(createTrackObject(4, Arrays.asList(0, 1, 2, 4, 8, 12), 90, 90));
+        tracks.add(createTrackObject(5, Arrays.asList(5, 10, 20), 120, 120));
+
+        extractFrames("samples/five-second-marathon-clip-numbered.mp4", tracks);
+    }
+
+    private void extractFrames(String resourcePath, List<JsonTrackOutputObject> tracks) throws IOException {
         URI resource = JniTestUtils.getFileResource(resourcePath);
 
         Path outputDirectory = tempFolder.newFolder().toPath().toAbsolutePath();
         FrameExtractor extractor = new FrameExtractor(resource, outputDirectory.toUri());
-        extractor.getFrames().addAll(frames);
-        extractor.execute();
+        extractor.getTracksToExtract().addAll(tracks);
+        Table<Integer, Integer, String> results = extractor.execute();
 
-        Set<String> fileNames;
-        try(Stream<Path> paths = Files.list(outputDirectory)) {
-            fileNames = paths.map(p -> p.getFileName().toString())
-                    .collect(toSet());
+        Set<String> fileNames = new TreeSet<>();
+        for (JsonTrackOutputObject track : tracks) {
+            Path trackPath = outputDirectory.resolve(track.getId());
+            try(Stream<Path> paths = Files.list(trackPath)) {
+                Set<String> tmpNames = paths.map(p -> p.getFileName().toString())
+                                .collect(toSet());
+                fileNames.addAll(tmpNames);
+            }
         }
-
+        List<Integer> frames = tracks.stream()
+                .flatMap(t -> t.getDetections().stream())
+                .map(d -> d.getOffsetFrame())
+                .collect(Collectors.toList());
         Assert.assertEquals(frames.size(), fileNames.size());
         frames.forEach(f -> Assert.assertTrue(fileNames.contains("frame-" + f + ".png")));
+
+        for (JsonTrackOutputObject track : tracks) {
+            track.getDetections().stream()
+            .forEach(d -> assertSizesMatch(d, results.get(Integer.parseInt(track.getId()), d.getOffsetFrame())));
+        }
+    }
+
+
+    private JsonTrackOutputObject createTrackObject(Integer trackId, List<Integer> frames,
+                                                    int width, int height) {
+        SortedSet<JsonDetectionOutputObject> detections = new TreeSet<>();
+        for (int i = 0; i < frames.size(); ++i) {
+            detections.add(new JsonDetectionOutputObject(0, 0, width, height,
+                    (float)0.0, Collections.emptySortedMap(), frames.get(i).intValue(),
+                    0, "NOT_ATTEMPTED", ""));
+        }
+        return new JsonTrackOutputObject(Integer.toString(trackId),
+                frames.get(0), frames.get(frames.size()-1), 0, 0, "", "", (float)0.0,
+                Collections.emptySortedMap(), detections.first(), detections);
+
+    }
+
+    private void assertSizesMatch(JsonDetectionOutputObject detection, String uri) {
+        int expectedWidth = detection.getWidth();
+        int expectedHeight = detection.getHeight();
+        int actualWidth = 0;
+        int actualHeight = 0;
+        File file = new File(uri);
+        try {
+            ImageInputStream imageStream = ImageIO.createImageInputStream(file);
+            Iterator<ImageReader> readers = ImageIO.getImageReaders(imageStream);
+            if (readers.hasNext()) {
+                ImageReader reader = readers.next();
+                reader.setInput(imageStream);
+                actualWidth = reader.getWidth(0);
+                actualHeight = reader.getHeight(0);
+            }
+        } catch (IOException e) {
+            Assert.fail(e.getMessage());
+        }
+        assertTrue((actualWidth == expectedWidth) && (actualHeight == expectedHeight));
     }
 }
+

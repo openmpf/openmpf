3f405eaa08ac4ee09170905c6aef1b0ba592d428
jrobble <jrobble@mitre.org>
2020-07-29 13:17:38 -0400

    Drop "duplicate paged in from cursor" DLQ messages.

--- trunk/workflow-manager/src/main/java/org/mitre/mpf/wfm/camel/routes/DlqRouteBuilder.java
+++ trunk/workflow-manager/src/main/java/org/mitre/mpf/wfm/camel/routes/DlqRouteBuilder.java
@@ -54,6 +54,7 @@ public class DlqRouteBuilder extends RouteBuilder {
 	public static final String DLQ_DELIVERY_FAILURE_CAUSE_PROPERTY = "dlqDeliveryFailureCause";
 
 	public static final String DUPLICATE_FROM_STORE_MESSAGE = "duplicate from store";
+	public static final String DUPLICATE_FROM_CURSOR_MESSAGE = "duplicate paged in from cursor";
 	public static final String SUPPRESSING_DUPLICATE_DELIVERY_MESSAGE = "Suppressing duplicate delivery";
 
 	private String entryPoint, exitPoint, auditExitPoint, invalidExitPoint, routeIdPrefix, selectorReplyTo;
@@ -82,6 +83,7 @@ public class DlqRouteBuilder extends RouteBuilder {
 
 		// Note that "LIKE" is case sensitive.
 		String dupCondition = DLQ_DELIVERY_FAILURE_CAUSE_PROPERTY + " LIKE '%" + DUPLICATE_FROM_STORE_MESSAGE + "%'" +
+				" OR " + DLQ_DELIVERY_FAILURE_CAUSE_PROPERTY + " LIKE '%" + DUPLICATE_FROM_CURSOR_MESSAGE + "%'" +
 				" OR " + DLQ_DELIVERY_FAILURE_CAUSE_PROPERTY + " LIKE '%" + SUPPRESSING_DUPLICATE_DELIVERY_MESSAGE + "%'";
 		String selector = "?selector=" + java.net.URLEncoder.encode(dupCondition, "UTF-8");
 

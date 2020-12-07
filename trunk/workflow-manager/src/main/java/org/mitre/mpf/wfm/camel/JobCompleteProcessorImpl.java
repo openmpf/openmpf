faafbfb65c519b272dfba9f46ac4ee72bf6701c4
brosenberg42 <brosenberg@mitre.org>
2020-12-07 17:58:31 -0500

    Add ability to censor property output (#1222)

--- trunk/workflow-manager/src/main/java/org/mitre/mpf/wfm/camel/JobCompleteProcessorImpl.java
+++ trunk/workflow-manager/src/main/java/org/mitre/mpf/wfm/camel/JobCompleteProcessorImpl.java
@@ -53,6 +53,7 @@ import org.mitre.mpf.wfm.enums.MpfHeaders;
 import org.mitre.mpf.wfm.event.JobCompleteNotification;
 import org.mitre.mpf.wfm.event.JobProgress;
 import org.mitre.mpf.wfm.event.NotificationConsumer;
+import org.mitre.mpf.wfm.service.CensorPropertiesService;
 import org.mitre.mpf.wfm.service.JobStatusBroadcaster;
 import org.mitre.mpf.wfm.service.StorageService;
 import org.mitre.mpf.wfm.util.*;
@@ -110,6 +111,9 @@ public class JobCompleteProcessorImpl extends WfmProcessor implements JobComplet
 
     @Autowired
     private JobStatusBroadcaster jobStatusBroadcaster;
+
+    @Autowired
+    private CensorPropertiesService censorPropertiesService;
 
 
     @Override
@@ -312,7 +316,7 @@ public class JobCompleteProcessorImpl extends WfmProcessor implements JobComplet
         JsonOutputObject jsonOutputObject = new JsonOutputObject(
                 jobRequest.getId(),
                 UUID.randomUUID().toString(),
-                jsonUtils.convert(transientJob.getPipeline()),
+                convertPipeline(transientJob.getPipeline()),
                 transientJob.getPriority(),
                 propertiesUtil.getSiteId(),
                 transientJob.getExternalId().orElse(null),
@@ -320,13 +324,16 @@ public class JobCompleteProcessorImpl extends WfmProcessor implements JobComplet
                 jobRequest.getTimeCompleted(),
                 jobStatus.getValue().toString());
 
-        if (transientJob.getOverriddenJobProperties() != null) {
-            jsonOutputObject.getJobProperties().putAll(transientJob.getOverriddenJobProperties());
+        censorPropertiesService.copyAndCensorProperties(
+                transientJob.getOverriddenJobProperties(), jsonOutputObject.getJobProperties());
+
+        for (Map.Entry<String, Map<String, String>> algoPropsEntry
+                : transientJob.getOverriddenAlgorithmProperties().rowMap().entrySet()) {
+            jsonOutputObject.getAlgorithmProperties().put(
+                    algoPropsEntry.getKey(),
+                    censorPropertiesService.copyAndCensorProperties(algoPropsEntry.getValue()));
         }
 
-        if (transientJob.getOverriddenAlgorithmProperties() != null) {
-            jsonOutputObject.getAlgorithmProperties().putAll(transientJob.getOverriddenAlgorithmProperties().rowMap());
-        }
         jsonOutputObject.getJobWarnings().addAll(transientJob.getWarnings());
         jsonOutputObject.getJobErrors().addAll(transientJob.getErrors());
 
@@ -341,7 +348,9 @@ public class JobCompleteProcessorImpl extends WfmProcessor implements JobComplet
                                                                                 transientMedia.isFailed() ? "ERROR" : "COMPLETE");
 
             mediaOutputObject.getMediaMetadata().putAll(transientMedia.getMetadata());
-            mediaOutputObject.getMediaProperties().putAll(transientMedia.getMediaSpecificProperties());
+            censorPropertiesService.copyAndCensorProperties(
+                    transientMedia.getMediaSpecificProperties(),
+                    mediaOutputObject.getMediaProperties());
 
             MarkupResult markupResult = markupResultDao.findByJobIdAndMediaIndex(jobId, mediaIndex);
             if(markupResult != null) {
@@ -445,10 +454,10 @@ public class JobCompleteProcessorImpl extends WfmProcessor implements JobComplet
     }
 
 
-    private static JsonTrackOutputObject createTrackOutputObject(Track track, String stateKey,
-                                                                 TransientAction transientAction,
-                                                                 TransientMedia transientMedia,
-                                                                 TransientJob transientJob) {
+    private JsonTrackOutputObject createTrackOutputObject(Track track, String stateKey,
+                                                          TransientAction transientAction,
+                                                          TransientMedia transientMedia,
+                                                          TransientJob transientJob) {
         JsonDetectionOutputObject exemplar = createDetectionOutputObject(track.getExemplar());
 
         AggregateJobPropertiesUtil.PropertyInfo exemplarsOnlyProp = AggregateJobPropertiesUtil.calculateValue(
@@ -492,24 +501,53 @@ public class JobCompleteProcessorImpl extends WfmProcessor implements JobComplet
                 track.getType(),
                 stateKey,
                 track.getConfidence(),
-                track.getTrackProperties(),
+                censorPropertiesService.copyAndCensorProperties(track.getTrackProperties()),
                 exemplar,
                 detections);
     }
 
 
-    private static JsonDetectionOutputObject createDetectionOutputObject(Detection detection) {
+    private JsonDetectionOutputObject createDetectionOutputObject(Detection detection) {
         return new JsonDetectionOutputObject(
                 detection.getX(),
                 detection.getY(),
                 detection.getWidth(),
                 detection.getHeight(),
                 detection.getConfidence(),
-                detection.getDetectionProperties(),
+                censorPropertiesService.copyAndCensorProperties(detection.getDetectionProperties(),
+                                                                new TreeMap<>()),
                 detection.getMediaOffsetFrame(),
                 detection.getMediaOffsetTime(),
                 detection.getArtifactExtractionStatus().name(),
                 detection.getArtifactPath());
+    }
+
+
+    private JsonPipeline convertPipeline(TransientPipeline transientPipeline) {
+        if (transientPipeline == null) {
+            return null;
+        }
+
+        JsonPipeline jsonPipeline = new JsonPipeline(transientPipeline.getName(),
+                                                     transientPipeline.getDescription());
+
+        for (TransientStage transientStage : transientPipeline.getStages()) {
+            JsonStage jsonStage = new JsonStage(transientStage.getActionType().name(),
+                                                transientStage.getName(),
+                                                transientStage.getDescription());
+
+            for (TransientAction transientAction : transientStage.getActions()) {
+                JsonAction jsonAction = new JsonAction(transientAction.getAlgorithm(),
+                                                       transientAction.getName(),
+                                                       transientAction.getDescription());
+                censorPropertiesService.copyAndCensorProperties(transientAction.getProperties(),
+                                                                jsonAction.getProperties());
+                jsonStage.getActions().add(jsonAction);
+            }
+
+            jsonPipeline.getStages().add(jsonStage);
+        }
+        return jsonPipeline;
     }
 
 
---
+++ trunk/workflow-manager/src/test/java/org/mitre/mpf/wfm/service/TestCensorPropertiesService.java
@@ -0,0 +1,123 @@
+/******************************************************************************
+ * NOTICE                                                                     *
+ *                                                                            *
+ * This software (or technical data) was produced for the U.S. Government     *
+ * under contract, and is subject to the Rights in Data-General Clause        *
+ * 52.227-14, Alt. IV (DEC 2007).                                             *
+ *                                                                            *
+ * Copyright 2020 The MITRE Corporation. All Rights Reserved.                 *
+ ******************************************************************************/
+
+/******************************************************************************
+ * Copyright 2020 The MITRE Corporation                                       *
+ *                                                                            *
+ * Licensed under the Apache License, Version 2.0 (the "License");            *
+ * you may not use this file except in compliance with the License.           *
+ * You may obtain a copy of the License at                                    *
+ *                                                                            *
+ *    http://www.apache.org/licenses/LICENSE-2.0                              *
+ *                                                                            *
+ * Unless required by applicable law or agreed to in writing, software        *
+ * distributed under the License is distributed on an "AS IS" BASIS,          *
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.   *
+ * See the License for the specific language governing permissions and        *
+ * limitations under the License.                                             *
+ ******************************************************************************/
+
+
+package org.mitre.mpf.wfm.service;
+
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSet;
+import org.junit.Before;
+import org.junit.Test;
+import org.mitre.mpf.wfm.util.PropertiesUtil;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.when;
+
+public class TestCensorPropertiesService {
+
+    @InjectMocks
+    private CensorPropertiesService _censorPropertiesService;
+
+    @Mock
+    private PropertiesUtil _mockPropertiesUtil;
+
+    @Before
+    public void init() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+
+    @Test
+    public void canCensorProperties() {
+        when(_mockPropertiesUtil.getCensoredOutputProperties())
+                .thenReturn(ImmutableSet.of("CENSORED1", "CENSORED2"));
+
+        Map<String, String> properties = ImmutableMap.of(
+                "PROP1", "VALUE1",
+                "CENSORED1", "CENSORED VALUE1",
+                "PROP2", "VALUE2",
+                "CENSORED2", "CENSORED VALUE2");
+
+        Map<String, String> censoredProperties
+                = _censorPropertiesService.copyAndCensorProperties(properties);
+
+        Map<String, String> expectedCensoredProperties = ImmutableMap.of(
+                "PROP1", "VALUE1",
+                "CENSORED1", CensorPropertiesService.CENSORED_PROP_REPLACEMENT,
+                "PROP2", "VALUE2",
+                "CENSORED2", CensorPropertiesService.CENSORED_PROP_REPLACEMENT);
+
+        assertEquals(expectedCensoredProperties, censoredProperties);
+    }
+
+
+    @Test
+    public void canHandleEmptyProps() {
+        when(_mockPropertiesUtil.getCensoredOutputProperties())
+                .thenReturn(ImmutableSet.of("CENSORED1", "CENSORED2"));
+
+        Map<String, String> censoredProperties
+                = _censorPropertiesService.copyAndCensorProperties(new HashMap<>());
+        assertTrue(censoredProperties.isEmpty());
+    }
+
+
+    @Test
+    public void canHandleNoCensoredOutputProps() {
+        when(_mockPropertiesUtil.getCensoredOutputProperties())
+                .thenReturn(ImmutableSet.of());
+
+        Map<String, String> properties = ImmutableMap.of(
+                "PROP1", "VALUE1",
+                "CENSORED1", "CENSORED VALUE1",
+                "PROP2", "VALUE2",
+                "CENSORED2", "CENSORED VALUE2");
+
+        Map<String, String> censoredProperties
+                = _censorPropertiesService.copyAndCensorProperties(properties);
+
+        assertEquals(properties, censoredProperties);
+    }
+
+
+    @Test
+    public void canHandleEmptyPropsAndNoCensoredOutputProps() {
+        when(_mockPropertiesUtil.getCensoredOutputProperties())
+                .thenReturn(ImmutableSet.of());
+
+        Map<String, String> censoredProperties
+                = _censorPropertiesService.copyAndCensorProperties(new HashMap<>());
+        assertTrue(censoredProperties.isEmpty());
+
+    }
+}

90a2ee79ca4cdca3c1a7b8a7c7d9b4fef2d13117
Jeff Robble <jrobble@mitre.org>
2020-12-18 14:57:13 -0500

    Update versions to 6.0. (#1235)

--- trunk/bin/mpf-scripts/setup.py
+++ trunk/bin/mpf-scripts/setup.py
@@ -27,7 +27,7 @@
 import setuptools
 
 setuptools.setup(name='mpf',
-                 version='5.1',
+                 version='6.0',
                  description='MPF management script',
                  packages=['mpf'],
                  entry_points={

51acf59168394c2016fe8497ddea8cd12da4caaa
Jeff Robble <jrobble@mitre.org>
2021-03-08 22:49:52 -0500

    Remove icon debug.

--- trunk/video-overlay/src/main/cpp/BoundingBoxImageHandle.h
+++ trunk/video-overlay/src/main/cpp/BoundingBoxImageHandle.h
@@ -35,8 +35,8 @@
 
 class BoundingBoxImageHandle {
 public:
-    static constexpr bool useIcons = true; // DEBUG: false;
-    static constexpr bool showFrameNumbers = true; // DEBUG: false;
+    static constexpr bool useIcons = false;
+    static constexpr bool showFrameNumbers = false;
 
     BoundingBoxImageHandle(std::string sourcePath, std::string destinationPath);
 

b08980ac027ab11f9794ba2c7f34b8a077ce420c
Brian Rosenberg <brosenberg@mitre.org>
2021-11-02 12:54:41 -0400

    Fix TestRemoteMediaProcessor

--- trunk/workflow-manager/src/test/java/org/mitre/mpf/wfm/camelOps/TestRemoteMediaProcessor.java
+++ trunk/workflow-manager/src/test/java/org/mitre/mpf/wfm/camelOps/TestRemoteMediaProcessor.java
@@ -33,6 +33,7 @@ import org.apache.camel.Message;
 import org.apache.camel.impl.DefaultMessage;
 import org.junit.*;
 import org.junit.rules.TemporaryFolder;
+import org.mitre.mpf.test.TestUtil;
 import org.mitre.mpf.wfm.camel.operations.mediaretrieval.RemoteMediaProcessor;
 import org.mitre.mpf.wfm.camel.operations.mediaretrieval.RemoteMediaSplitter;
 import org.mitre.mpf.wfm.data.InProgressBatchJobsService;
@@ -49,8 +50,10 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import spark.Spark;
 
 import java.net.URI;
+import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Collections;
 import java.util.List;
@@ -65,7 +68,7 @@ import static org.mockito.Mockito.*;
 public class TestRemoteMediaProcessor {
     private static final Logger LOG = LoggerFactory.getLogger(TestRemoteMediaProcessor.class);
     private static final int MINUTES = 1000*60; // 1000 milliseconds/second & 60 seconds/minute.
-    private static final String EXT_IMG = "https://raw.githubusercontent.com/openmpf/openmpf/master/trunk/mpf-system-tests/src/test/resources/samples/face/meds-aa-S001-01.jpg";
+    private static final String EXT_IMG = "http://localhost:4587/test-image.jpg";
 
     private RemoteMediaProcessor _remoteMediaProcessor;
 
@@ -85,6 +88,12 @@ public class TestRemoteMediaProcessor {
     @BeforeClass
     public static void initClass() {
         setHttpProxies();
+        startSpark();
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+        Spark.stop();
     }
 
     private static void setHttpProxies() {
@@ -214,5 +223,18 @@ public class TestRemoteMediaProcessor {
 
     private Exchange setupExchange(long jobId, MediaImpl media) {
         return MediaTestUtil.setupExchange(jobId, media, _mockInProgressJobs);
+    }
+
+
+    private static void startSpark() {
+        Spark.port(4587);
+        Spark.get("/test-image.jpg", (req, resp) -> {
+            var path = Paths.get(TestUtil.findFile("/samples/meds1.jpg"));
+            try (var out = resp.raw().getOutputStream()) {
+                Files.copy(path, out);
+            }
+            resp.raw().flushBuffer();
+            return "";
+        });
     }
 }

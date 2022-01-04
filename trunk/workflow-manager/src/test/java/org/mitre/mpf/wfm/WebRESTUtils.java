f6b2f67c5d27ca2a6c07a319fbe8173701a5157b
Brian Rosenberg <brosenberg@mitre.org>
2022-01-04 10:40:36 -0500

    Fix last .json and remove atomikos

--- trunk/workflow-manager/src/test/java/org/mitre/mpf/wfm/WebRESTUtils.java
+++ trunk/workflow-manager/src/test/java/org/mitre/mpf/wfm/WebRESTUtils.java
@@ -26,9 +26,7 @@
 
 package org.mitre.mpf.wfm;
 
-import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.http.NameValuePair;
@@ -72,7 +70,7 @@ public class WebRESTUtils {
 	}
 
 	public static JsonNode getNodes() throws MalformedURLException, JsonProcessingException {
-		String url = REST_URL + "nodes/info.json";
+		String url = REST_URL + "nodes/info";
 		log.debug("getNodes get {}",url);
 		var obj = objectMapper.readTree(getJSON(new URL(url), MPF_AUTHORIZATION));
 		return obj.get("nodeModels");
@@ -226,20 +224,21 @@ public class WebRESTUtils {
 		return sb.toString();
 	}
 
-	public static SingleJobInfo getSingleJobInfo(long jobId) throws JsonParseException, JsonMappingException, IOException {
-		String urlJobsStatus = REST_URL + "jobs/" + Long.toString(jobId) + ".json";
+	public static SingleJobInfo getSingleJobInfo(long jobId) throws IOException {
+		String urlJobsStatus = REST_URL + "jobs/" + jobId;
 		String jsonJobResponse = getJSON(new URL(urlJobsStatus), MPF_AUTHORIZATION);
-		Assert.assertTrue("Failed to retrieve JSON when GETting job info for job id: " + Long.toString(jobId), jsonJobResponse.length() >= 0);
+		Assert.assertFalse("Failed to retrieve JSON when GETting job info for job id: " + jobId,
+		                   jsonJobResponse.isEmpty());
 		return objectMapper.readValue(jsonJobResponse, SingleJobInfo.class);
 	}
 
-	public static BatchJobStatusType getJobsStatus(long jobid)throws JsonParseException, JsonMappingException, IOException  {
+	public static BatchJobStatusType getJobsStatus(long jobid) throws IOException  {
 		SingleJobInfo singleJobInfo = getSingleJobInfo(jobid);
 		//convert to the enum and return
 		return BatchJobStatusType.valueOf(singleJobInfo.getJobStatus());
 	}
 
-	public static boolean waitForJobToTerminate(long jobid, long delay) throws InterruptedException, JsonParseException, JsonMappingException, IOException {
+	public static boolean waitForJobToTerminate(long jobid, long delay) throws InterruptedException, IOException {
 		log.info("[waitForJobToTerminate] job {}, delay:{} ", jobid, delay);
 		int count=20;
 		BatchJobStatusType status;
@@ -250,7 +249,6 @@ public class WebRESTUtils {
 			count--;
 		}
 		while(count > 0 && !status.isTerminal());
-		if(count > 0) return true;
-		return false;
+		return count > 0;
 	}
 }

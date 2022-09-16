02b82c8a51036a8ce6caf07a267452018614e519
brosenberg42 <brosenberg@mitre.org>
2022-09-16 08:45:25 -0400

    Feature/long markup label (#1579)

--- trunk/workflow-manager/src/test/java/org/mitre/mpf/wfm/camel/operations/markup/TestMarkupSplitter.java
+++ trunk/workflow-manager/src/test/java/org/mitre/mpf/wfm/camel/operations/markup/TestMarkupSplitter.java
@@ -77,8 +77,10 @@ public class TestMarkupSplitter {
                                       NUMERIC_PROP_NAME, "9.999"),
                 ImmutableSortedMap.of(TEXT_PROP_NAME, "abc",
                                       NUMERIC_PROP_NAME, "1.11111"));
-        Assert.assertEquals("prefix: some reall 9.999",
-                            MarkupSplitter.getLabel(track, "prefix: ", TEXT_PROP_NAME, NUMERIC_PROP_NAME).get());
+        Assert.assertEquals(
+                "prefix: some reall 9.999",
+                MarkupSplitter.getLabel(track, "prefix: ", TEXT_PROP_NAME, 10,
+                                        NUMERIC_PROP_NAME).get());
     }
 
     @Test
@@ -86,8 +88,10 @@ public class TestMarkupSplitter {
         Detection detection = createDetection(
                 ImmutableSortedMap.of(TEXT_PROP_NAME, "abc",
                                       NUMERIC_PROP_NAME, "1.11111"));
-        Assert.assertEquals("prefix: abc 1.111",
-                            MarkupSplitter.getLabel(detection, "prefix: ", TEXT_PROP_NAME, NUMERIC_PROP_NAME).get());
+        Assert.assertEquals(
+                "prefix: abc 1.111",
+                MarkupSplitter.getLabel(detection, "prefix: ", TEXT_PROP_NAME, 10,
+                                        NUMERIC_PROP_NAME).get());
     }
 
     @Test
@@ -97,24 +101,30 @@ public class TestMarkupSplitter {
                     ImmutableSortedMap.of(),
                     ImmutableSortedMap.of(TEXT_PROP_NAME, "abc",
                                           NUMERIC_PROP_NAME, "1.11111"));
-            Assert.assertEquals("abc 1.111",
-                                MarkupSplitter.getLabel(track, "", TEXT_PROP_NAME, NUMERIC_PROP_NAME).get());
+            Assert.assertEquals(
+                    "abc 1.111",
+                    MarkupSplitter.getLabel(track, "", TEXT_PROP_NAME, 10,
+                                            NUMERIC_PROP_NAME).get());
         }
         {
             Track track = createTrack(
                     ImmutableSortedMap.of(TEXT_PROP_NAME, "some really really long class"),
                     ImmutableSortedMap.of(TEXT_PROP_NAME, "abc",
                                           NUMERIC_PROP_NAME, "1.11111"));
-            Assert.assertEquals("some reall 1.111",
-                                MarkupSplitter.getLabel(track, "", TEXT_PROP_NAME, NUMERIC_PROP_NAME).get());
+            Assert.assertEquals(
+                    "some reall 1.111",
+                    MarkupSplitter.getLabel(track, "", TEXT_PROP_NAME, 10,
+                                            NUMERIC_PROP_NAME).get());
         }
         {
             Track track = createTrack(
                     ImmutableSortedMap.of(NUMERIC_PROP_NAME, "9.999"),
                     ImmutableSortedMap.of(TEXT_PROP_NAME, "abc",
                                           NUMERIC_PROP_NAME, "1.11111"));
-            Assert.assertEquals("abc 9.999",
-                                MarkupSplitter.getLabel(track, "", TEXT_PROP_NAME, NUMERIC_PROP_NAME).get());
+            Assert.assertEquals(
+                    "abc 9.999",
+                    MarkupSplitter.getLabel(track, "", TEXT_PROP_NAME, 10,
+                                            NUMERIC_PROP_NAME).get());
         }
     }
 
@@ -126,24 +136,63 @@ public class TestMarkupSplitter {
                                           NUMERIC_PROP_NAME, "9.99999"),
                     ImmutableSortedMap.of(TEXT_PROP_NAME, "abc",
                                           NUMERIC_PROP_NAME, "7000.5555"));
-            Assert.assertEquals("some reall 10.000",
-                                MarkupSplitter.getLabel(track, "", TEXT_PROP_NAME, NUMERIC_PROP_NAME).get());
+            Assert.assertEquals(
+                    "some reall 10.000",
+                    MarkupSplitter.getLabel(track, "", TEXT_PROP_NAME, 10,
+                                            NUMERIC_PROP_NAME).get());
         }
         {
             Detection detection = createDetection(
                     ImmutableSortedMap.of(TEXT_PROP_NAME, "abc",
                                           NUMERIC_PROP_NAME, "7000.5555"));
-            Assert.assertEquals("abc 7000.556",
-                                MarkupSplitter.getLabel(detection, "", TEXT_PROP_NAME, NUMERIC_PROP_NAME).get());
+            Assert.assertEquals(
+                    "abc 7000.556",
+                    MarkupSplitter.getLabel(detection, "", TEXT_PROP_NAME, 10,
+                                            NUMERIC_PROP_NAME).get());
         }
         {
             Detection detection = createDetection(
                     ImmutableSortedMap.of(TEXT_PROP_NAME, "abcdefgh   cutoff",
                                           NUMERIC_PROP_NAME, "7000.5"));
-            Assert.assertEquals("abcdefgh 7000.500",
-                                MarkupSplitter.getLabel(detection, "", TEXT_PROP_NAME, NUMERIC_PROP_NAME).get());
+            Assert.assertEquals(
+                    "abcdefgh 7000.500",
+                    MarkupSplitter.getLabel(detection, "", TEXT_PROP_NAME, 10,
+                                            NUMERIC_PROP_NAME).get());
         }
     }
+
+
+    @Test
+    public void testLabelLength() {
+        Detection detection = createDetection(
+                ImmutableSortedMap.of(TEXT_PROP_NAME, "some really really long class",
+                                      NUMERIC_PROP_NAME, "7000.5555"));
+
+        Assert.assertEquals(
+                "7000.556",
+                MarkupSplitter.getLabel(detection, "", TEXT_PROP_NAME, 0,
+                                        NUMERIC_PROP_NAME).get());
+
+        Assert.assertEquals(
+                "s 7000.556",
+                MarkupSplitter.getLabel(detection, "", TEXT_PROP_NAME, 1,
+                                        NUMERIC_PROP_NAME).get());
+        Assert.assertEquals(
+                "some really 7000.556",
+                MarkupSplitter.getLabel(detection, "", TEXT_PROP_NAME, 11,
+                                        NUMERIC_PROP_NAME).get());
+
+        Assert.assertEquals(
+                "some really really long class 7000.556",
+                MarkupSplitter.getLabel(detection, "", TEXT_PROP_NAME, 29,
+                                        NUMERIC_PROP_NAME).get());
+
+        Assert.assertEquals(
+                "some really really long class 7000.556",
+                MarkupSplitter.getLabel(detection, "", TEXT_PROP_NAME, 100,
+                                        NUMERIC_PROP_NAME).get());
+    }
+
 
     @Test
     public void getLabelWithMissingPart() {
@@ -151,35 +200,44 @@ public class TestMarkupSplitter {
             Track track = createTrack(
                     ImmutableSortedMap.of(),
                     ImmutableSortedMap.of());
-            Assert.assertTrue(MarkupSplitter.getLabel(track, "", TEXT_PROP_NAME, NUMERIC_PROP_NAME).isEmpty());
+            Assert.assertTrue(MarkupSplitter.getLabel(
+                    track, "", TEXT_PROP_NAME, 10, NUMERIC_PROP_NAME).isEmpty());
         }
         {
             Track track = createTrack(
                     ImmutableSortedMap.of(TEXT_PROP_NAME, "some really really long class"),
                     ImmutableSortedMap.of());
-            Assert.assertEquals("some reall",
-                                MarkupSplitter.getLabel(track, "", TEXT_PROP_NAME, NUMERIC_PROP_NAME).get());
+            Assert.assertEquals(
+                    "some reall",
+                    MarkupSplitter.getLabel(track, "", TEXT_PROP_NAME, 10,
+                                            NUMERIC_PROP_NAME).get());
         }
         {
             Track track = createTrack(
                     ImmutableSortedMap.of(NUMERIC_PROP_NAME, "9.999"),
                     ImmutableSortedMap.of());
-            Assert.assertEquals("9.999",
-                                MarkupSplitter.getLabel(track, "", TEXT_PROP_NAME, NUMERIC_PROP_NAME).get());
+            Assert.assertEquals(
+                    "9.999",
+                    MarkupSplitter.getLabel(track, "", TEXT_PROP_NAME, 10,
+                                            NUMERIC_PROP_NAME).get());
         }
         {
             Track track = createTrack(
                     ImmutableSortedMap.of(),
                     ImmutableSortedMap.of(TEXT_PROP_NAME, "abc"));
-            Assert.assertEquals("abc",
-                                MarkupSplitter.getLabel(track, "", TEXT_PROP_NAME, NUMERIC_PROP_NAME).get());
+            Assert.assertEquals(
+                    "abc",
+                    MarkupSplitter.getLabel(track, "", TEXT_PROP_NAME, 10,
+                                            NUMERIC_PROP_NAME).get());
         }
         {
             Track track = createTrack(
                     ImmutableSortedMap.of(),
                     ImmutableSortedMap.of(NUMERIC_PROP_NAME, "1.11111"));
-            Assert.assertEquals("1.111",
-                                MarkupSplitter.getLabel(track, "", TEXT_PROP_NAME, NUMERIC_PROP_NAME).get());
+            Assert.assertEquals(
+                    "1.111",
+                    MarkupSplitter.getLabel(track, "", TEXT_PROP_NAME, 10,
+                                            NUMERIC_PROP_NAME).get());
         }
     }
 
@@ -191,15 +249,19 @@ public class TestMarkupSplitter {
                                           NUMERIC_PROP_NAME, "9.999"),
                     ImmutableSortedMap.of(TEXT_PROP_NAME, "abc",
                                           NUMERIC_PROP_NAME, "1.11111"));
-            Assert.assertEquals("some reall 7.778",
-                               MarkupSplitter.getLabel(track, "", TEXT_PROP_NAME, "CONFIDENCE").get());
+            Assert.assertEquals(
+                    "some reall 7.778",
+                    MarkupSplitter.getLabel(track, "", TEXT_PROP_NAME, 10,
+                                            "CONFIDENCE").get());
         }
         {
             Detection detection = createDetection(
                     ImmutableSortedMap.of(TEXT_PROP_NAME, "abc",
                                           NUMERIC_PROP_NAME, "1.11111"));
-            Assert.assertEquals("abc 8.889",
-                                MarkupSplitter.getLabel(detection, "", TEXT_PROP_NAME, "CONFIDENCE").get());
+            Assert.assertEquals(
+                    "abc 8.889",
+                    MarkupSplitter.getLabel(detection, "", TEXT_PROP_NAME, 10,
+                                            "CONFIDENCE").get());
         }
         {
             Track track = createTrack(
@@ -207,15 +269,19 @@ public class TestMarkupSplitter {
                                           "CONFIDENCE", "9.999"),
                     ImmutableSortedMap.of(TEXT_PROP_NAME, "abc",
                                           "CONFIDENCE", "1.11111"));
-            Assert.assertEquals("some reall 9.999",
-                                MarkupSplitter.getLabel(track, "", TEXT_PROP_NAME, "CONFIDENCE").get());
+            Assert.assertEquals(
+                    "some reall 9.999",
+                    MarkupSplitter.getLabel(track, "", TEXT_PROP_NAME, 10,
+                                            "CONFIDENCE").get());
         }
         {
             Detection detection = createDetection(
                     ImmutableSortedMap.of(TEXT_PROP_NAME, "abc",
                                           "CONFIDENCE", "1.11111"));
-            Assert.assertEquals("abc 1.111",
-                                MarkupSplitter.getLabel(detection, "", TEXT_PROP_NAME, "CONFIDENCE").get());
+            Assert.assertEquals(
+                    "abc 1.111",
+                    MarkupSplitter.getLabel(detection, "", TEXT_PROP_NAME, 10,
+                                            "CONFIDENCE").get());
         }
     }
 
@@ -227,15 +293,19 @@ public class TestMarkupSplitter {
                                           NUMERIC_PROP_NAME, "9.999"),
                     ImmutableSortedMap.of(TEXT_PROP_NAME, "abc",
                                           NUMERIC_PROP_NAME, "1.11111"));
-            Assert.assertEquals("9.999",
-                                MarkupSplitter.getLabel(track, "", "", NUMERIC_PROP_NAME).get());
+            Assert.assertEquals(
+                    "9.999",
+                    MarkupSplitter.getLabel(track, "", "", 10,
+                                            NUMERIC_PROP_NAME).get());
         }
         {
             Detection detection = createDetection(
                     ImmutableSortedMap.of(TEXT_PROP_NAME, "abc",
                                           NUMERIC_PROP_NAME, "1.11111"));
-            Assert.assertEquals("1.111",
-                                MarkupSplitter.getLabel(detection, "", "", NUMERIC_PROP_NAME).get());
+            Assert.assertEquals(
+                    "1.111",
+                    MarkupSplitter.getLabel(detection, "", "", 10,
+                                            NUMERIC_PROP_NAME).get());
         }
         {
             Track track = createTrack(
@@ -243,15 +313,17 @@ public class TestMarkupSplitter {
                                           NUMERIC_PROP_NAME, "9.999"),
                     ImmutableSortedMap.of(TEXT_PROP_NAME, "abc",
                                           NUMERIC_PROP_NAME, "1.11111"));
-            Assert.assertEquals("some reall",
-                                MarkupSplitter.getLabel(track, "", TEXT_PROP_NAME, "").get());
+            Assert.assertEquals(
+                    "some reall",
+                    MarkupSplitter.getLabel(track, "", TEXT_PROP_NAME, 10, "").get());
         }
         {
             Detection detection = createDetection(
                     ImmutableSortedMap.of(TEXT_PROP_NAME, "abc",
                                           NUMERIC_PROP_NAME, "1.11111"));
-            Assert.assertEquals("abc",
-                                MarkupSplitter.getLabel(detection, "", TEXT_PROP_NAME, "").get());
+            Assert.assertEquals(
+                    "abc",
+                    MarkupSplitter.getLabel(detection, "", TEXT_PROP_NAME, 10, "").get());
         }
         {
             Track track = createTrack(
@@ -259,13 +331,13 @@ public class TestMarkupSplitter {
                                           NUMERIC_PROP_NAME, "9.999"),
                     ImmutableSortedMap.of(TEXT_PROP_NAME, "abc",
                                           NUMERIC_PROP_NAME, "1.11111"));
-            Assert.assertTrue(MarkupSplitter.getLabel(track, "", "", "").isEmpty());
+            Assert.assertTrue(MarkupSplitter.getLabel(track, "", "", 10, "").isEmpty());
         }
         {
             Detection detection = createDetection(
                     ImmutableSortedMap.of(TEXT_PROP_NAME, "abc",
                                           NUMERIC_PROP_NAME, "1.11111"));
-            Assert.assertTrue(MarkupSplitter.getLabel(detection, "", "", "").isEmpty());
+            Assert.assertTrue(MarkupSplitter.getLabel(detection, "", "", 10, "").isEmpty());
         }
     }
 
@@ -278,15 +350,19 @@ public class TestMarkupSplitter {
                     ImmutableSortedMap.of(TEXT_PROP_NAME, "abc",
                                           NUMERIC_PROP_NAME, "1.11111"));
 
-            Assert.assertEquals("some reall",
-                                MarkupSplitter.getLabel(track, "", TEXT_PROP_NAME, NUMERIC_PROP_NAME).get());
+            Assert.assertEquals(
+                    "some reall",
+                    MarkupSplitter.getLabel(track, "", TEXT_PROP_NAME, 10,
+                                            NUMERIC_PROP_NAME).get());
         }
         {
             Detection detection = createDetection(
                     ImmutableSortedMap.of(TEXT_PROP_NAME, "abc",
                                           NUMERIC_PROP_NAME, "not a number"));
-            Assert.assertEquals("abc",
-                                MarkupSplitter.getLabel(detection, "", TEXT_PROP_NAME, NUMERIC_PROP_NAME).get());
+            Assert.assertEquals(
+                    "abc",
+                    MarkupSplitter.getLabel(detection, "", TEXT_PROP_NAME, 10,
+                                            NUMERIC_PROP_NAME).get());
         }
     }
 }

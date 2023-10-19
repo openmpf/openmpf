9214b8691e91f044abac0987aa8234f3897da4bc
Brian Rosenberg <brosenberg@mitre.org>
2023-10-19 09:25:33 -0400

    Refactor markup amq interaction

--- trunk/markup/src/main/java/org/mitre/mpf/markup/MarkupRequestConsumer.java
+++ trunk/markup/src/main/java/org/mitre/mpf/markup/MarkupRequestConsumer.java
@@ -26,8 +26,22 @@
 
 package org.mitre.mpf.markup;
 
-import com.google.common.base.Stopwatch;
-import com.google.common.io.Files;
+import static java.util.stream.Collectors.toMap;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.net.URI;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Optional;
+
+import javax.jms.BytesMessage;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageProducer;
+import javax.jms.Session;
+
 import org.mitre.mpf.videooverlay.BoundingBox;
 import org.mitre.mpf.videooverlay.BoundingBoxMap;
 import org.mitre.mpf.videooverlay.BoundingBoxSource;
@@ -35,72 +49,134 @@ import org.mitre.mpf.videooverlay.BoundingBoxWriter;
 import org.mitre.mpf.wfm.buffers.Markup;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.jms.core.JmsTemplate;
-import org.springframework.jms.core.MessageCreator;
-import org.springframework.stereotype.Service;
 
-import javax.annotation.PostConstruct;
-import javax.jms.*;
-import java.io.File;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.io.StringWriter;
-import java.net.URI;
-import java.util.Enumeration;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Optional;
-import java.util.concurrent.TimeUnit;
+import com.google.protobuf.InvalidProtocolBufferException;
 
-import static java.util.stream.Collectors.toMap;
+public class MarkupRequestConsumer {
 
-@Service(MarkupRequestConsumer.REF)
-public class MarkupRequestConsumer implements MessageListener {
-    public static final String REF = "markupRequestConsumer";
-    private static final Logger log = LoggerFactory.getLogger(MarkupRequestConsumer.class);
+    private static final Logger LOG = LoggerFactory.getLogger(MarkupRequestConsumer.class);
+
+    private final Session _session;
+
+    private final MessageProducer _messageProducer;
 
     static {
-        log.debug("Loading JNI library.");
+        LOG.debug("Loading JNI library.");
         System.loadLibrary("mpfopencvjni");
     }
 
-    @PostConstruct
-    public void init() {
-        log.info("Markup initialization complete");
+    public MarkupRequestConsumer(Session session) throws JMSException {
+        _session = session;
+        _messageProducer = session.createProducer(null);
     }
 
 
-    @Autowired
-    @Qualifier("markupResponseTemplate")
-    private JmsTemplate markupResponseTemplate;
+    public void onMessage(Message message) {
+        long startTimeMs = System.currentTimeMillis();
 
-    private Markup.MarkupResponse.Builder initializeResponse(Markup.MarkupRequest request) {
-        Markup.MarkupResponse.Builder responseBuilder = Markup.MarkupResponse.newBuilder()
+        Markup.MarkupRequest markupRequest;
+        Markup.MarkupResponse.Builder responseBuilder;
+        try {
+            markupRequest = getMarkupRequest(message);
+            responseBuilder = initializeResponse(message, markupRequest);
+        }
+        catch (Exception e) {
+            LOG.error("Unable to parse the MarkupRequest protobuf due to: " + e, e);
+            rollback();
+            return;
+        }
+
+        try {
+            LOG.info("Processing markup request. Media ID = {}, Type = {}, "
+                    + "Number of detections = {}, Source URI = \"{}\", Destination URI = \"{}\".",
+                    markupRequest.getMediaId(), markupRequest.getMediaType(),
+                    markupRequest.getMapEntriesCount(),
+                    markupRequest.getSourceUri(), markupRequest.getDestinationUri());
+            onMarkupRequest(markupRequest);
+            responseBuilder.setOutputFileUri(markupRequest.getDestinationUri());
+        }
+        catch (Exception e) {
+            LOG.error("Markup failed due to: " + e, e);
+            setErrorFields(responseBuilder, e);
+        }
+        responseBuilder.setTimeProcessing(System.currentTimeMillis() - startTimeMs);
+
+        try {
+            var response = responseBuilder.build();
+            sendResponse(message, response);
+            if (!response.getHasError()) {
+                LOG.info("Markup request for media {} completed successfully.",
+                        markupRequest.getMediaId());
+            }
+        }
+        catch (Exception e) {
+            LOG.error("Sending markup response failed due to: " + e, e);
+            rollback();
+        }
+    }
+
+    private Markup.MarkupRequest getMarkupRequest(Message message)
+            throws JMSException, InvalidProtocolBufferException {
+        // The caller will catch the possible ClassCastException and handle it.
+        var bytesMessage = (BytesMessage) message;
+        var protobytes = new byte[(int) bytesMessage.getBodyLength()];
+        bytesMessage.readBytes(protobytes);
+        return Markup.MarkupRequest.parseFrom(protobytes);
+    }
+
+
+    private static Markup.MarkupResponse.Builder initializeResponse(
+            Message message, Markup.MarkupRequest request) {
+        var result = Markup.MarkupResponse.newBuilder()
                 .setMediaIndex(request.getMediaIndex())
                 .setMediaId(request.getMediaId())
                 .setRequestId(request.getRequestId())
                 .setTaskIndex(request.getTaskIndex())
                 .setActionIndex(request.getActionIndex())
                 .setHasError(false);
-        return responseBuilder;
+        try {
+            result.setRequestTimestamp(message.getJMSTimestamp());
+        }
+        catch (JMSException e) {
+            // Ignored because setting the timestamp is not required.
+        }
+        return result;
     }
 
-    private void finishWithError(Markup.MarkupResponse.Builder markupResponseBuilder, Exception exception) {
-        markupResponseBuilder.setHasError(true);
-        markupResponseBuilder.setErrorMessage(exception.getMessage());
-
-        StringWriter writer = new StringWriter(5120);
-        exception.printStackTrace(new PrintWriter(writer));
-        markupResponseBuilder.setErrorStackTrace(writer.toString());
+    private void rollback() {
+        try {
+            // Rollback is used in cases where it is not possible to create a
+            // Markup.MarkupResponse protobuf message. The invalid request messages will end up
+            // in a dead letter queue after redelivery fails.
+            _session.rollback();
+        }
+        catch (JMSException e) {
+            throw new IllegalStateException("Rollback failed due to: " + e, e);
+        }
     }
 
-    private static boolean markup(Markup.MarkupRequest markupRequest) throws IOException {
-        log.info("[Markup Request #{}] Source: '{}' Destination: '{}'.",
-                markupRequest.getRequestId(), markupRequest.getSourceUri(), markupRequest.getDestinationUri());
+    private void onMarkupRequest(Markup.MarkupRequest markupRequest) throws IOException {
+        var destinationPath = Path.of(URI.create(markupRequest.getDestinationUri()));
+        if (!Files.isWritable(destinationPath)) {
+            throw new IllegalStateException(
+                    "The target URI '%s' is not writable."
+                    .formatted(markupRequest.getDestinationUri()));
+        }
 
-        BoundingBoxWriter writer = new BoundingBoxWriter();
+        var sourcePath = Path.of(URI.create(markupRequest.getSourceUri()));
+        if (!Files.isReadable(sourcePath)) {
+            throw new IllegalStateException(
+                    "The target URI '%s' is not readable."
+                    .formatted(markupRequest.getSourceUri()));
+        }
+        if (markupRequest.getMapEntriesList().isEmpty()) {
+            Files.copy(sourcePath, destinationPath);
+        }
+        markup(markupRequest);
+    }
+
+    private static void markup(Markup.MarkupRequest markupRequest) throws IOException {
+        var writer = new BoundingBoxWriter();
         writer.setSourceMedium(URI.create(markupRequest.getSourceUri()));
         writer.setDestinationMedium(URI.create(markupRequest.getDestinationUri()));
 
@@ -115,153 +191,64 @@ public class MarkupRequestConsumer implements MessageListener {
                                Markup.MarkupRequestPropertyMap::getValue));
         writer.setRequestProperties(requestProperties);
 
-        BoundingBoxMap map = new BoundingBoxMap();
-        int boxesAdded = 0;
-        for(Markup.BoundingBoxMapEntry boundingBoxMapEntry : markupRequest.getMapEntriesList()) {
-            Optional<String> label = boundingBoxMapEntry.getBoundingBox().hasLabel() ?
-                    Optional.of(boundingBoxMapEntry.getBoundingBox().getLabel()) : Optional.empty();
-            BoundingBox boundingBox = new BoundingBox(
-                    boundingBoxMapEntry.getBoundingBox().getX(),
-                    boundingBoxMapEntry.getBoundingBox().getY(),
-                    boundingBoxMapEntry.getBoundingBox().getWidth(),
-                    boundingBoxMapEntry.getBoundingBox().getHeight(),
-                    boundingBoxMapEntry.getBoundingBox().getRotationDegrees(),
-                    boundingBoxMapEntry.getBoundingBox().getFlip(),
-                    boundingBoxMapEntry.getBoundingBox().getRed(),
-                    boundingBoxMapEntry.getBoundingBox().getGreen(),
-                    boundingBoxMapEntry.getBoundingBox().getBlue(),
-                    BoundingBoxSource.valueOf(boundingBoxMapEntry.getBoundingBox().getSource().toString()),
-                    boundingBoxMapEntry.getBoundingBox().getMoving(),
-                    boundingBoxMapEntry.getBoundingBox().getExemplar(),
+        var map = new BoundingBoxMap();
+        for (var boundingBoxMapEntry : markupRequest.getMapEntriesList()) {
+            var srcBox = boundingBoxMapEntry.getBoundingBox();
+            var label = srcBox.hasLabel()
+                    ? Optional.of(srcBox.getLabel())
+                    : Optional.<String>empty();
+            var dstBox = new BoundingBox(
+                    srcBox.getX(),
+                    srcBox.getY(),
+                    srcBox.getWidth(),
+                    srcBox.getHeight(),
+                    srcBox.getRotationDegrees(),
+                    srcBox.getFlip(),
+                    srcBox.getRed(),
+                    srcBox.getGreen(),
+                    srcBox.getBlue(),
+                    BoundingBoxSource.valueOf(srcBox.getSource().toString()),
+                    srcBox.getMoving(),
+                    srcBox.getExemplar(),
                     label);
-            map.putOnFrame(boundingBoxMapEntry.getFrameNumber(), boundingBox);
-            boxesAdded++;
+            map.putOnFrame(boundingBoxMapEntry.getFrameNumber(), dstBox);
         }
 
-        log.info("[Markup Request #{}] Marking up {} detections on '{}'.",
-                markupRequest.getRequestId(), markupRequest.getMapEntriesCount(), markupRequest.getDestinationUri());
-
-        if(boxesAdded > 0) {
-            writer.setBoundingBoxMap(map);
-            if (markupRequest.getMediaType() == Markup.MediaType.IMAGE) {
-                writer.markupImage();
-            } else {
-                writer.markupVideo();
-            }
+        writer.setBoundingBoxMap(map);
+        if (markupRequest.getMediaType() == Markup.MediaType.IMAGE) {
+            writer.markupImage();
         }
-
-        return boxesAdded > 0;
+        else {
+            writer.markupVideo();
+        }
     }
 
-    public void onMessage(Message message) {
-        Stopwatch stopwatch = Stopwatch.createStarted();
-
-        if(message == null) {
-            log.warn("Received a null JMS message. No action will be taken.");
-            return;
+    private static void setErrorFields(
+            Markup.MarkupResponse.Builder responseBuilder, Exception exception) {
+        responseBuilder.setHasError(true);
+        responseBuilder.setErrorMessage("Markup failed due to: " + exception);
+        var writer = new StringWriter();
+        try (var pw = new PrintWriter(writer)) {
+            exception.printStackTrace(pw);
         }
+        responseBuilder.setErrorStackTrace(writer.toString());
+    }
 
-        if(!(message instanceof BytesMessage)) {
-            log.warn("Received a JMS message, but it was not of the correct type. No action will be taken.");
-            return;
-        }
 
-        try {
-            log.info("Received JMS message. Type = {}. JMS Message ID = {}. JMS Correlation ID = {}.",
-                    message.getClass().getName(), message.getJMSMessageID(), message.getJMSCorrelationID());
+    private void sendResponse(Message requestMessage, Markup.MarkupResponse markupResponse)
+            throws JMSException {
+        var responseMessage = _session.createBytesMessage();
+        responseMessage.setJMSPriority(requestMessage.getJMSPriority());
+        responseMessage.writeBytes(markupResponse.toByteArray());
 
-            final Map<String, Object> requestHeaders = new HashMap<String, Object>();
-            Enumeration<String> properties = message.getPropertyNames();
-
-            while (properties.hasMoreElements()) {
-                String propertyName = properties.nextElement();
-                requestHeaders.put(propertyName, message.getObjectProperty(propertyName));
+        var propertyNames = requestMessage.getPropertyNames();
+        while (propertyNames.hasMoreElements()) {
+            if (propertyNames.nextElement() instanceof String propName) {
+                var propValue = requestMessage.getObjectProperty(propName);
+                responseMessage.setObjectProperty(propName, propValue);
             }
-
-            byte[] messageBytes = new byte[(int)(((BytesMessage)message).getBodyLength())];
-            ((BytesMessage)message).readBytes(messageBytes);
-            Markup.MarkupRequest markupRequest = Markup.MarkupRequest.parseFrom(messageBytes);
-            Markup.MarkupResponse.Builder markupResponseBuilder = initializeResponse(markupRequest);
-            markupResponseBuilder.setRequestTimestamp(message.getJMSTimestamp());
-
-            log.debug("Processing markup request. Media Index = {}. Media ID = {} (type = {}). Request ID = {}.",
-                    markupRequest.getMediaIndex(), markupRequest.getMediaId(), markupRequest.getMediaType(),
-                    markupRequest.getRequestId());
-
-	        try {
-		        if (!new File(URI.create(markupRequest.getDestinationUri())).canWrite()) {
-			        throw new Exception();
-		        }
-	        } catch (Exception exception) {
-		        markupResponseBuilder.setHasError(true);
-		        markupResponseBuilder.setErrorMessage(String.format("The target URI '%s' is not writable.",
-                        markupRequest.getDestinationUri()));
-	        }
-
-	        try {
-		        if (!new File(URI.create(markupRequest.getSourceUri())).canRead()) {
-			        throw new Exception();
-		        }
-	        } catch (Exception exception) {
-		        markupResponseBuilder.setHasError(true);
-		        markupResponseBuilder.setErrorMessage(String.format("The source URI '%s' is not readable.",
-                        markupRequest.getSourceUri()));
-	        }
-
-	        if(!markupResponseBuilder.getHasError()) {
-		        if (markupRequest.getMapEntriesCount() == 0) {
-			        try {
-                        String sourceUri = markupRequest.getSourceUri();
-                        String sourceExt = Files.getFileExtension(sourceUri);
-			            String destinationUri = markupRequest.getDestinationUri();
-			            String destinationExt = Files.getFileExtension(destinationUri);
-			            if (destinationExt.isEmpty() && !sourceExt.isEmpty()) {
-                            destinationUri += "." + sourceExt;
-                        }
-				        Files.copy(new File(URI.create(sourceUri)), new File(URI.create(destinationUri)));
-				        markupResponseBuilder.setOutputFileUri(destinationUri);
-			        } catch (Exception exception) {
-				        log.error("Failed to mark up the file '{}' because of an exception.",
-                                                markupRequest.getSourceUri(), exception);
-				        finishWithError(markupResponseBuilder, exception);
-			        }
-		        } else {
-			        try {
-				        if (markup(markupRequest)) {
-					        markupResponseBuilder.setOutputFileUri(markupRequest.getDestinationUri());
-				        } else {
-					        markupResponseBuilder.setOutputFileUri(markupRequest.getSourceUri());
-				        }
-			        } catch (Exception exception) {
-				        log.error("Failed to mark up the file '{}' because of an exception.",
-                                                markupRequest.getSourceUri(), exception);
-				        finishWithError(markupResponseBuilder, exception);
-			        }
-		        }
-	        }
-
-            stopwatch.stop();
-            markupResponseBuilder.setTimeProcessing(stopwatch.elapsed(TimeUnit.MILLISECONDS));
-            final Markup.MarkupResponse markupResponse = markupResponseBuilder.build();
-
-	    log.info("Returning response for Media {}. Error: {}.",
-                    markupResponse.getMediaId(), markupResponse.getHasError());
-            markupResponseTemplate.setSessionTransacted(true);
-            markupResponseTemplate.setDefaultDestination(message.getJMSReplyTo());
-            markupResponseTemplate.setPriority(message.getJMSPriority());
-            markupResponseTemplate.send(new MessageCreator() {
-                public Message createMessage(Session session) throws JMSException {
-                    BytesMessage bytesMessage = session.createBytesMessage();
-                    bytesMessage.writeBytes(markupResponse.toByteArray());
-                    for (Map.Entry<String, Object> entry : requestHeaders.entrySet()) {
-                        bytesMessage.setObjectProperty(entry.getKey(), entry.getValue());
-                    }
-                    return bytesMessage;
-                }
-            });
-
-        } catch (Exception e) {
-            log.error(e.getMessage(), e);
         }
+        _messageProducer.send(requestMessage.getJMSReplyTo(), responseMessage);
+        _session.commit();
     }
 }

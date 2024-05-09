c6ad2cc4ed1d28cd8959308006ed6287b0bdf6af
jrobble <jrobble@mitre.org>
2024-05-08 21:32:37 -0400

    Update versions to 9.0.

--- trunk/bin/mpf-scripts/setup.py
+++ trunk/bin/mpf-scripts/setup.py
@@ -27,7 +27,7 @@
 import setuptools
 
 setuptools.setup(name='mpf',
-                 version='8.0',
+                 version='9.0',
                  description='MPF management script',
                  packages=['mpf'],
                  entry_points={

5bce7e654a5bd74ddd75eee7e436cdef448913df
Jeff Robble <jrobble@mitre.org>
2024-05-24 21:47:16 -0400

    Update copyright to 2024. (#1811)

--- trunk/bin/mpf-scripts/mpf/command_line.py
+++ trunk/bin/mpf-scripts/mpf/command_line.py
@@ -6,11 +6,11 @@
 # under contract, and is subject to the Rights in Data-General Clause       #
 # 52.227-14, Alt. IV (DEC 2007).                                            #
 #                                                                           #
-# Copyright 2023 The MITRE Corporation. All Rights Reserved.                #
+# Copyright 2024 The MITRE Corporation. All Rights Reserved.                #
 #############################################################################
 
 #############################################################################
-# Copyright 2023 The MITRE Corporation                                      #
+# Copyright 2024 The MITRE Corporation                                      #
 #                                                                           #
 # Licensed under the Apache License, Version 2.0 (the "License");           #
 # you may not use this file except in compliance with the License.          #

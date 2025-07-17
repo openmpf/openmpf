f95195777b8618a618e31dc926bcf5870dd098a8
cnowacki <cnowacki@mitre.org>
2025-07-17 16:43:34 -0400

    working HeifConverter

--- trunk/video-overlay/src/main/cpp/HeifConverter.cpp
+++ trunk/video-overlay/src/main/cpp/HeifConverter.cpp
@@ -25,10 +25,26 @@
  ******************************************************************************/
 
 #include <jni.h>
-#include <libheif/heif_cxx.h>
-#include <libheif/heifio/encoder_png.h>
-
 #include "JniHelper.h"
+//#include <libheif/heif.h>
+#include <libheif/heifio/encoder.h>
+#include <libheif/heifio/encoder_png.h>
+#include <libheif/heif_cxx.h>
+
+class LibHeifInitializer {
+    public:
+    LibHeifInitializer() { heif_init(nullptr); }
+    ~LibHeifInitializer() { heif_deinit(); }
+};
+
+class ContextReleaser {
+public:
+  ContextReleaser(struct heif_context* ctx) : ctx_(ctx) {}
+  ~ContextReleaser() {heif_context_free(ctx_);}
+private:
+  struct heif_context* ctx_;
+};
+
 
 extern "C" {
 
@@ -37,22 +53,40 @@ JNIEXPORT void JNICALL Java_org_mitre_mpf_heif_HeifConverter_convertNative (
 
     JniHelper jni(env);
 
-    class LibHeifInitializer {
-        public:
-        LibHeifInitializer() { heif_init(nullptr); }
-        ~LibHeifInitializer() { heif_deinit(); }
-    };
-
     try {
         LibHeifInitializer initializer;
-        heif::Context ctx;
-        ctx.read_from_file(jni.ToStdString(inputFile));
-
+        struct heif_context* ctx = heif_context_alloc();
+        ContextReleaser cr(ctx);
+        struct heif_error err;
+        err = heif_context_read_from_file(ctx, jni.ToStdString(inputFile).c_str(), nullptr);
+        if (err.code != 0) {
+            throw std::runtime_error(std::string("Could not read HEIF/AVIF file: ") + std::string(err.message));
+        }
+        struct heif_image_handle* handle;
+        err = heif_context_get_primary_image_handle(ctx, &handle);
+        if (err.code != 0) {
+            throw std::runtime_error(std::string("Could not get HEIF/AVIF primary image handle: ") + std::string(err.message));
+        }
         std::unique_ptr<PngEncoder> encoder = std::make_unique<PngEncoder>();
 
-        heif::ImageHandle handle = ctx.get_primary_image_handle();
-        heif::ImageHandle png_img = ctx.encode_image(handle, encoder);
-        ctx.write_to_file(jni.ToStdString(outputFile));
+        struct heif_image* image;
+        int has_alpha = heif_image_handle_has_alpha_channel(handle);
+        int bit_depth = heif_image_handle_get_luma_bits_per_pixel(handle);
+        if (bit_depth < 0) {
+            throw std::runtime_error("HEIF/AVIF image has undefined bit-depth");
+        }
+
+        err = heif_decode_image(handle, &image,
+                                encoder->colorspace(has_alpha),
+                                encoder->chroma(has_alpha, bit_depth),
+                                nullptr);
+        if (err.code != 0) {
+            throw std::runtime_error(std::string("Could not decode HEIF/AVIF image: ") + std::string(err.message));
+        }
+        bool success = encoder->Encode(handle, image, jni.ToStdString(outputFile));
+        if (!success) {
+            throw std::runtime_error("Could not encode HEIF/AVIF image to PNG image");
+        }
     }
     catch (const std::exception &e) {
         jni.ReportCppException(e.what());

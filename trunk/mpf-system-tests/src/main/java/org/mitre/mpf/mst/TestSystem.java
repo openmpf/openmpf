7e5eaf1fc14f1d668c50a977253b09ef037e2a29
John Tat <jtat@mitre.org>
2025-07-22 11:03:56 -0400

    print actual output for job

--- trunk/mpf-system-tests/src/main/java/org/mitre/mpf/mst/TestSystem.java
+++ trunk/mpf-system-tests/src/main/java/org/mitre/mpf/mst/TestSystem.java
@@ -419,6 +419,7 @@ public abstract class TestSystem {
                 expectedOutputPath, jobId);
         var expectedOutputJson = objectMapper.readValue(expectedOutputPath, JsonOutputObject.class);
         var actualOutputJson = getJobOutputObject(jobId);
+        log.info("Actual output for job {}: {}", jobId, actualOutputJson);
         outputChecker.compareJsonOutputObjects(
                 expectedOutputJson, actualOutputJson, jobRequest.pipelineName());
         return actualOutputJson;

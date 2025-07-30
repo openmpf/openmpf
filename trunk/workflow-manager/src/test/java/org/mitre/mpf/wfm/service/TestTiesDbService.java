965800109c68b1dfa5e312d75eb82f36627a2ad9
John Tat <jtat@mitre.org>
2025-07-30 14:48:21 -0400

    update failing tests

--- trunk/workflow-manager/src/test/java/org/mitre/mpf/wfm/service/TestTiesDbService.java
+++ trunk/workflow-manager/src/test/java/org/mitre/mpf/wfm/service/TestTiesDbService.java
@@ -557,7 +557,7 @@ public class TestTiesDbService extends MockitoTest.Strict {
         var dataObject = new TiesDbInfo.DataObject(
                 "TEST_PIPELINE",
                 ImmutableSortedSet.of(
-                        "PARENT_SUPPRESSED", "MERGE_SOURCE", "MERGE_SOURCE_AND_TARGET",
+                         "MERGE_SOURCE", "MERGE_SOURCE_AND_TARGET",
                         "MERGE_SOURCE0", "MERGE_SOURCE1"),
                 "host-123",
                 "file:///fake-path",
